b8cb8766cca4c57db4e90cba9b403acdc4199254
Clement Escoffier <clement.escoffier@gmail.com>
2018-12-04 18:24:48 +0100

    Add another bean to illustrate that the callbacks can access beans.

--- application-lifecycle-events/src/main/java/org/acme/events/AppLifecycleBean.java
+++ application-lifecycle-events/src/main/java/org/acme/events/AppLifecycleBean.java
@@ -2,6 +2,7 @@ package org.acme.events;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
+import javax.inject.Inject;
 
 import org.jboss.shamrock.runtime.ShutdownEvent;
 import org.jboss.shamrock.runtime.StartupEvent;
@@ -9,16 +10,21 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @ApplicationScoped
-public class ApplicationListenerBean {
+public class AppLifecycleBean {
 
     private static final Logger LOGGER = LoggerFactory.getLogger("ListenerBean");
 
+    /**
+     * Inject a bean used in the callbacks.
+     */
+    @Inject MyOtherBean bean;
+
     void onStart(@Observes StartupEvent ev) {
-        LOGGER.info("The application is starting...");          
+        LOGGER.info("The application is starting...{}", bean.hello());          
     }
 
     void onStop(@Observes ShutdownEvent ev) {
-        LOGGER.info("The application is stopping...");
+        LOGGER.info("The application is stopping... {}", bean.bye());
     }
     
 }
