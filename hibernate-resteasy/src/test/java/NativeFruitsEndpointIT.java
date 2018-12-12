6dbf0f1e6ba50a85eec31639b3daafb935047f3c
Sanne Grinovero <sanne@hibernate.org>
2018-12-12 11:41:17 +0000

    Import the REST-CRUD demo as a quickstart

---
+++ hibernate-resteasy/src/test/java/NativeFruitsEndpointIT.java
@@ -0,0 +1,8 @@
+import org.jboss.shamrock.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeFruitsEndpointIT extends FruitsEndpointTest {
+
+    // Runs the same tests as the parent class
+
+}
