51d4755da0cadc4bb37c688c813dc4de5180841a
Gary Brown <gary@brownuk.com>
2019-01-16 09:21:57 +0000

    Add using-opentracing quickstart

---
+++ using-opentracing/src/test/java/org/acme/opentracing/TracedResourceTest.java
@@ -0,0 +1,22 @@
+package org.acme.opentracing;
+
+import org.jboss.shamrock.test.ShamrockTest;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@RunWith(ShamrockTest.class)
+public class TracedResourceTest {
+
+    @Test
+    public void testHelloEndpoint() {
+        given()
+          .when().get("/app/hello")
+          .then()
+             .statusCode(200)
+             .body(is("hello"));
+    }
+
+}
