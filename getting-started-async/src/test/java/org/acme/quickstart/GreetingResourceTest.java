2f9b317881a65c7eb632f94868b2a1e94c9c69b6
Stuart Douglas <stuart.w.douglas@gmail.com>
2019-01-23 13:44:13 +1100

    Move quickstarts to JUnit 5
    Requires Shamrock 0.7.0

--- getting-started-async/src/test/java/org/acme/quickstart/GreetingResourceTest.java
+++ getting-started-async/src/test/java/org/acme/quickstart/GreetingResourceTest.java
@@ -1,15 +1,14 @@
 package org.acme.quickstart;
 
-import org.jboss.shamrock.test.ShamrockTest;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.jboss.shamrock.test.junit.ShamrockTest;
+import org.junit.jupiter.api.Test;
 
 import java.util.UUID;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
-@RunWith(ShamrockTest.class)    
+@ShamrockTest
 public class GreetingResourceTest {
 
     @Test
@@ -17,7 +16,7 @@ public class GreetingResourceTest {
         given()
           .when().get("/hello")
           .then()
-             .statusCode(200)  
+             .statusCode(200)
              .body(is("hello"));
     }
 
