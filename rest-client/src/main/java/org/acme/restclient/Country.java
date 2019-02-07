55c52e6a7a5549fe0ce8fbd22b28ca5c2a0d0cd2
Georgios Andrianakis <geoand@gmail.com>
2019-02-07 22:15:01 +0200

    Slim model to make it more readable

--- rest-client/src/main/java/org/acme/restclient/Country.java
+++ rest-client/src/main/java/org/acme/restclient/Country.java
@@ -5,29 +5,9 @@ import java.util.List;
 public class Country {
 
     private String name;
-    private List<String> topLevelDomain;
     private String alpha2Code;
-    private String alpha3Code;
-    private List<String> callingCodes;
     private String capital;
-    private List<String> altSpellings;
-    private String region;
-    private String subregion;
-    private Integer population;
-    private List<Double> latlng;
-    private String demonym;
-    private Double area;
-    private Double gini;
-    private List<String> timezones;
-    private List<String> borders;
-    private String nativeName;
-    private String numericCode;
     private List<Currency> currencies;
-    private List<Language> languages;
-    private Translations translations;
-    private String flag;
-    private List<RegionalBloc> regionalBlocs;
-    private String cioc;
 
     public String getName() {
         return name;
@@ -35,14 +15,6 @@ public class Country {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public List<String> getTopLevelDomain() {
-        return topLevelDomain;
-    }
-
-    public void setTopLevelDomain(List<String> topLevelDomain) {
-        this.topLevelDomain = topLevelDomain;
     }
 
     public String getAlpha2Code() {
@@ -53,22 +25,6 @@ public class Country {
         this.alpha2Code = alpha2Code;
     }
 
-    public String getAlpha3Code() {
-        return alpha3Code;
-    }
-
-    public void setAlpha3Code(String alpha3Code) {
-        this.alpha3Code = alpha3Code;
-    }
-
-    public List<String> getCallingCodes() {
-        return callingCodes;
-    }
-
-    public void setCallingCodes(List<String> callingCodes) {
-        this.callingCodes = callingCodes;
-    }
-
     public String getCapital() {
         return capital;
     }
@@ -77,148 +33,12 @@ public class Country {
         this.capital = capital;
     }
 
-    public List<String> getAltSpellings() {
-        return altSpellings;
-    }
-
-    public void setAltSpellings(List<String> altSpellings) {
-        this.altSpellings = altSpellings;
-    }
-
-    public String getRegion() {
-        return region;
-    }
-
-    public void setRegion(String region) {
-        this.region = region;
-    }
-
-    public String getSubregion() {
-        return subregion;
-    }
-
-    public void setSubregion(String subregion) {
-        this.subregion = subregion;
-    }
-
-    public Integer getPopulation() {
-        return population;
-    }
-
-    public void setPopulation(Integer population) {
-        this.population = population;
-    }
-
-    public List<Double> getLatlng() {
-        return latlng;
-    }
-
-    public void setLatlng(List<Double> latlng) {
-        this.latlng = latlng;
-    }
-
-    public String getDemonym() {
-        return demonym;
-    }
-
-    public void setDemonym(String demonym) {
-        this.demonym = demonym;
-    }
-
-    public Double getArea() {
-        return area;
-    }
-
-    public void setArea(Double area) {
-        this.area = area;
-    }
-
-    public Double getGini() {
-        return gini;
-    }
-
-    public void setGini(Double gini) {
-        this.gini = gini;
-    }
-
-    public List<String> getTimezones() {
-        return timezones;
-    }
-
-    public void setTimezones(List<String> timezones) {
-        this.timezones = timezones;
-    }
-
-    public List<String> getBorders() {
-        return borders;
-    }
-
-    public void setBorders(List<String> borders) {
-        this.borders = borders;
-    }
-
-    public String getNativeName() {
-        return nativeName;
-    }
-
-    public void setNativeName(String nativeName) {
-        this.nativeName = nativeName;
-    }
-
-    public String getNumericCode() {
-        return numericCode;
-    }
-
-    public void setNumericCode(String numericCode) {
-        this.numericCode = numericCode;
-    }
-
     public List<Currency> getCurrencies() {
         return currencies;
     }
 
     public void setCurrencies(List<Currency> currencies) {
         this.currencies = currencies;
-    }
-
-    public List<Language> getLanguages() {
-        return languages;
-    }
-
-    public void setLanguages(List<Language> languages) {
-        this.languages = languages;
-    }
-
-    public Translations getTranslations() {
-        return translations;
-    }
-
-    public void setTranslations(Translations translations) {
-        this.translations = translations;
-    }
-
-    public String getFlag() {
-        return flag;
-    }
-
-    public void setFlag(String flag) {
-        this.flag = flag;
-    }
-
-    public List<RegionalBloc> getRegionalBlocs() {
-        return regionalBlocs;
-    }
-
-    public void setRegionalBlocs(List<RegionalBloc> regionalBlocs) {
-        this.regionalBlocs = regionalBlocs;
-    }
-
-    public String getCioc() {
-        return cioc;
-    }
-
-    public void setCioc(String cioc) {
-        this.cioc = cioc;
     }
 
     public static class Currency {
@@ -251,174 +71,4 @@ public class Country {
         }
     }
 
-    public static class Language {
-        private String iso639_1;
-        private String iso639_2;
-        private String name;
-        private String nativeName;
-
-        public String getIso639_1() {
-            return iso639_1;
-        }
-
-        public void setIso639_1(String code) {
-            this.iso639_1 = code;
-        }
-
-        public String getIso639_2() {
-            return iso639_2;
-        }
-
-        public void setIso639_2(String iso639_2) {
-            this.iso639_2 = iso639_2;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public String getNativeName() {
-            return nativeName;
-        }
-
-        public void setNativeName(String nativeName) {
-            this.nativeName = nativeName;
-        }
-    }
-
-    public static class Translations {
-        private String de;
-        private String es;
-        private String fr;
-        private String ja;
-        private String it;
-        private String br;
-        private String pt;
-        private String nl;
-        private String hr;
-        private String fa;
-
-        public String getDe() {
-            return de;
-        }
-
-        public void setDe(String de) {
-            this.de = de;
-        }
-
-        public String getEs() {
-            return es;
-        }
-
-        public void setEs(String es) {
-            this.es = es;
-        }
-
-        public String getFr() {
-            return fr;
-        }
-
-        public void setFr(String fr) {
-            this.fr = fr;
-        }
-
-        public String getJa() {
-            return ja;
-        }
-
-        public void setJa(String ja) {
-            this.ja = ja;
-        }
-
-        public String getIt() {
-            return it;
-        }
-
-        public void setIt(String it) {
-            this.it = it;
-        }
-
-        public String getBr() {
-            return br;
-        }
-
-        public void setBr(String br) {
-            this.br = br;
-        }
-
-        public String getPt() {
-            return pt;
-        }
-
-        public void setPt(String pt) {
-            this.pt = pt;
-        }
-
-        public String getNl() {
-            return nl;
-        }
-
-        public void setNl(String nl) {
-            this.nl = nl;
-        }
-
-        public String getHr() {
-            return hr;
-        }
-
-        public void setHr(String hr) {
-            this.hr = hr;
-        }
-
-        public String getFa() {
-            return fa;
-        }
-
-        public void setFa(String fa) {
-            this.fa = fa;
-        }
-    }
-
-    public static class RegionalBloc {
-        private String acronym;
-        private String name;
-        private List<String> otherAcronyms;
-        private List<String> otherNames;
-
-        public String getAcronym() {
-            return acronym;
-        }
-
-        public void setAcronym(String acronym) {
-            this.acronym = acronym;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public List<String> getOtherAcronyms() {
-            return otherAcronyms;
-        }
-
-        public void setOtherAcronyms(List<String> otherAcronyms) {
-            this.otherAcronyms = otherAcronyms;
-        }
-
-        public List<String> getOtherNames() {
-            return otherNames;
-        }
-
-        public void setOtherNames(List<String> otherNames) {
-            this.otherNames = otherNames;
-        }
-    }
 }
