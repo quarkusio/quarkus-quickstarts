d8e94b8e5aaea905b643e873c2e1eb0fc624c932
Clement Escoffier <clement.escoffier@gmail.com>
2019-02-28 16:52:46 +0100

    Initial version of the Kafka quickstart

---
+++ kafka-quickstart/src/main/java/org/acme/quarkus/sample/PriceConverter.java
@@ -0,0 +1,25 @@
+package org.acme.quarkus.sample;
+
+import io.smallrye.reactive.messaging.annotations.Broadcast;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.eclipse.microprofile.reactive.messaging.Outgoing;
+
+import javax.enterprise.context.ApplicationScoped;
+
+/**
+ * A bean consuming data from the "prices" Kafka topic and applying some conversion.
+ * The result is pushed to the "my-data-stream" stream which is an in-memory stream.
+ */
+@ApplicationScoped
+public class PriceConverter {
+
+    private static final double CONVERSION_RATE = 0.88;
+
+    @Incoming("prices")
+    @Outgoing("my-data-stream")
+    @Broadcast
+    public double process(int priceInUsd) {
+        return priceInUsd * CONVERSION_RATE;
+    }
+
+}
