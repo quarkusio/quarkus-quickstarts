0b0ceafebfbc1b104d6af0ddd1c6b53f56dac375
Daniel Petisme <daniel.petisme@gmail.com>
2019-03-26 22:25:25 +0100

    Add an OpenAPI/Swagger UI quickstart

---
+++ using-openapi-swaggerui/src/main/java/org/acme/openapi/swaggerui/FruitResource.java
@@ -0,0 +1,37 @@
+package org.acme.openapi.swaggerui;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Set;
+
+@Path("/fruits")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class FruitResource {
+
+    private Set<Fruit> fruits = Collections.newSetFromMap(Collections.synchronizedMap(new LinkedHashMap<>()));
+
+    public FruitResource() {
+        fruits.add(new Fruit("Apple", "Winter fruit"));
+        fruits.add(new Fruit("Pineapple", "Tropical fruit"));
+    }
+
+    @GET
+    public Set<Fruit> list() {
+        return fruits;
+    }
+
+    @POST
+    public Set<Fruit> add(Fruit fruit) {
+        fruits.add(fruit);
+        return fruits;
+    }
+
+    @DELETE
+    public Set<Fruit> delete(Fruit fruit) {
+        fruits.remove(fruit);
+        return fruits;
+    }
+}
---
+++ using-openapi-swaggerui/src/test/java/org/acme/openapi/swaggerui/NativeFruitResourceIT.java
@@ -0,0 +1,9 @@
+package org.acme.openapi.swaggerui;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeFruitResourceIT extends FruitResourceTest {
+
+    // Execute the same tests but in native mode.
+}
