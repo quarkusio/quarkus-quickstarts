86e0d45fdb819d34853e5e2b619782ba54561c2a
Pavol Loffay <ploffay@redhat.com>
2019-04-12 17:30:14 +0200

    Add CDI and rest client to opentracing example
    Signed-off-by: Pavol Loffay <ploffay@redhat.com>

--- using-opentracing/src/main/java/org/acme/opentracing/TracedResource.java
+++ using-opentracing/src/main/java/org/acme/opentracing/TracedResource.java
@@ -1,16 +1,39 @@
 package org.acme.opentracing;
 
+import java.net.URI;
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriInfo;
+import org.eclipse.microprofile.rest.client.RestClientBuilder;
 
-@Path("/hello")
+@Path("/")
 public class TracedResource {
 
+    @Inject
+    private FrancophoneService exampleBean;
+
+    @Context
+    private UriInfo uriInfo;
+
     @GET
+    @Path("/hello")
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "hello";
+    }
+
+    @GET
+    @Path("/chain")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String chain() {
+        ResourceClient resourceClient = RestClientBuilder.newBuilder()
+            .baseUri(uriInfo.getBaseUri())
+            .build(ResourceClient.class);
+        return "chain -> " + exampleBean.bonjour() + " -> " + resourceClient.hello();
     }
 }
