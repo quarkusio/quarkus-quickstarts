796a1e112198762a23b16eb45075592ddf9a47a7
Pavol Loffay <ploffay@redhat.com>
2019-04-12 17:30:14 +0200

    Add CDI and rest client to opentracing example
    Signed-off-by: Pavol Loffay <ploffay@redhat.com>

---
+++ using-opentracing/src/main/java/org/acme/opentracing/FrancophoneService.java
@@ -0,0 +1,14 @@
+package org.acme.opentracing;
+
+
+import javax.enterprise.context.ApplicationScoped;
+import org.eclipse.microprofile.opentracing.Traced;
+
+@Traced
+@ApplicationScoped
+public class FrancophoneService {
+
+  public String bonjour() {
+    return "bonjour";
+  }
+}
