e70073a117f3e6215a3753eb89454410bcb60e3a
Clement Escoffier <clement.escoffier@gmail.com>
2019-06-06 08:54:56 +0200

    Apply changes from PR  #154
    * Update test containers
    * Remove useless modifiers

--- hibernate-orm-resteasy/src/test/java/org/acme/hibernate/orm/FruitsEndpointTest.java
+++ hibernate-orm-resteasy/src/test/java/org/acme/hibernate/orm/FruitsEndpointTest.java
@@ -4,14 +4,32 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.core.IsNot.not;
 
+import com.github.dockerjava.api.model.ExposedPort;
+import com.github.dockerjava.api.model.PortBinding;
+import com.github.dockerjava.api.model.Ports;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @QuarkusTest
-public class FruitsEndpointTest {
+@Testcontainers
+class FruitsEndpointTest {
+
+    @Container
+    static PostgreSQLContainer DATABASE = new PostgreSQLContainer<>("postgres:10.5")
+            .withDatabaseName("quarkus_test")
+            .withUsername("quarkus_test")
+            .withPassword("quarkus_test")
+            .withExposedPorts(5432)
+            .withCreateContainerCmdModifier(cmd ->
+                    cmd
+                    .withHostName("localhost")
+                    .withPortBindings(new PortBinding(Ports.Binding.bindPort(5432), new ExposedPort(5432))));
 
     @Test
-    public void testListAllFruits() {
+    void testListAllFruits() {
         //List all, should have all 3 fruits the database has initially:
         given()
               .when().get("/fruits")
