9bb789d802e7e39c45d928a88f89dfdfa653b1bd
Manyanda Chitimbo <manyanda.chitimbo@gmail.com>
2019-07-08 12:43:13 +0200

    feat(hibernate): terminates sql statements with a semicolon
    See https://github.com/quarkusio/quarkus/issues/2854#issuecomment-509149183

--- hibernate-orm-panache-resteasy/src/main/resources/import.sql
+++ hibernate-orm-panache-resteasy/src/main/resources/import.sql
@@ -1,3 +1,3 @@
-INSERT INTO fruit(id, name) VALUES (nextval('hibernate_sequence'), 'Cherry')
-INSERT INTO fruit(id, name) VALUES (nextval('hibernate_sequence'), 'Apple')
-INSERT INTO fruit(id, name) VALUES (nextval('hibernate_sequence'), 'Banana')
+INSERT INTO fruit(id, name) VALUES (nextval('hibernate_sequence'), 'Cherry');
+INSERT INTO fruit(id, name) VALUES (nextval('hibernate_sequence'), 'Apple');
+INSERT INTO fruit(id, name) VALUES (nextval('hibernate_sequence'), 'Banana');
