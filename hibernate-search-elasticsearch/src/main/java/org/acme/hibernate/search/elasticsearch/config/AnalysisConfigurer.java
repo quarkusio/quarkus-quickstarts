a414c2e88a5c9ba39462ee32ea55dfc4517b5fd4
Rostislav Svoboda <rsvoboda@redhat.com>
2019-08-29 10:21:34 +0200

    Fix AnalysisConfigurer to implement updated ElasticsearchAnalysisConfigurer

--- hibernate-search-elasticsearch/src/main/java/org/acme/hibernate/search/elasticsearch/config/AnalysisConfigurer.java
+++ hibernate-search-elasticsearch/src/main/java/org/acme/hibernate/search/elasticsearch/config/AnalysisConfigurer.java
@@ -1,12 +1,12 @@
 package org.acme.hibernate.search.elasticsearch.config;
 
+import org.hibernate.search.backend.elasticsearch.analysis.ElasticsearchAnalysisConfigurationContext;
 import org.hibernate.search.backend.elasticsearch.analysis.ElasticsearchAnalysisConfigurer;
-import org.hibernate.search.backend.elasticsearch.analysis.model.dsl.ElasticsearchAnalysisDefinitionContainerContext;
 
 public class AnalysisConfigurer implements ElasticsearchAnalysisConfigurer {
 
     @Override
-    public void configure(ElasticsearchAnalysisDefinitionContainerContext context) {
+    public void configure(ElasticsearchAnalysisConfigurationContext context) {
         context.analyzer("name").custom()
                 .withTokenizer("standard")
                 .withTokenFilters("asciifolding", "lowercase");
