2a195fb62e1890d8827de9dac1337e8d7160426b
Yoann Rodière <yoann@hibernate.org>
2019-10-09 15:55:53 +0200

    Avoid using fetchAllHits in the Hibernate Search quickstart
    It's really not something you would use in a real-world application.

--- hibernate-search-elasticsearch/src/main/java/org/acme/hibernate/search/elasticsearch/LibraryResource.java
+++ hibernate-search-elasticsearch/src/main/java/org/acme/hibernate/search/elasticsearch/LibraryResource.java
@@ -1,6 +1,7 @@
 package org.acme.hibernate.search.elasticsearch;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
@@ -107,7 +108,8 @@ public class LibraryResource {
     @GET
     @Path("author/search")
     @Transactional
-    public List<Author> searchAuthors(@QueryParam("pattern") String pattern) {
+    public List<Author> searchAuthors(@QueryParam("pattern") String pattern,
+            @QueryParam("size") Optional<Integer> size) {
         return Search.session(em)
                 .search(Author.class)
                 .predicate(f ->
@@ -117,6 +119,6 @@ public class LibraryResource {
                                 .fields("firstName", "lastName", "books.title").matching(pattern)
                 )
                 .sort(f -> f.field("lastName_sort").then().field("firstName_sort"))
-                .fetchAllHits();
+                .fetchHits(size.orElse(20));
     }
 }
