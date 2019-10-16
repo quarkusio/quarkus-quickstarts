ddcc7cbd0c332a0fe094e8e88441504ce9ab5826
Clement Escoffier <clement.escoffier@gmail.com>
2019-10-16 09:24:07 +0200

    Introduce a handshake mechanism to avoid a race condition.

--- using-websockets/src/test/java/org/acme/websocket/ChatTestCase.java
+++ using-websockets/src/test/java/org/acme/websocket/ChatTestCase.java
@@ -20,7 +20,7 @@ public class ChatTestCase {
 
     @Test
     public void testWebsocketChat() throws Exception {
-        try(Session session = ContainerProvider.getWebSocketContainer().connectToServer(Client.class, uri)) {
+        try (Session session = ContainerProvider.getWebSocketContainer().connectToServer(Client.class, uri)) {
             Assertions.assertEquals("CONNECT", MESSAGES.poll(10, TimeUnit.SECONDS));
             Assertions.assertEquals("User stu joined", MESSAGES.poll(10, TimeUnit.SECONDS));
             session.getAsyncRemote().sendText("hello world");
@@ -32,8 +32,11 @@ public class ChatTestCase {
     public static class Client {
 
         @OnOpen
-        public void open() {
+        public void open(Session session) {
             MESSAGES.add("CONNECT");
+            // Send a message to indicate that we are ready,
+            // as the message handler may not be registered immediately after this callback.
+            session.getAsyncRemote().sendText("_ready_");
         }
 
         @OnMessage
