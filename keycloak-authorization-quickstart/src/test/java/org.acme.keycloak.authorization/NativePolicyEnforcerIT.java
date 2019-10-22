da9cf67316af682f44ccfc1d79c3c1bd7929d85d
Pedro Igor <pigor.craveiro@gmail.com>
2019-10-22 19:34:10 -0300

    [fixes #4495] - Quarkus Keycloak Authorization Extension

---
+++ keycloak-authorization-quickstart/src/test/java/org.acme.keycloak.authorization/NativePolicyEnforcerIT.java
@@ -0,0 +1,10 @@
+package org.acme.keycloak.authorization;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+/**
+ * @author <a href="mailto:psilva@redhat.com">Pedro Igor</a>
+ */
+@SubstrateTest
+public class NativePolicyEnforcerIT extends PolicyEnforcerTest {
+}
