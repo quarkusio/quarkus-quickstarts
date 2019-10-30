2276ec32ddbe48e2f165da60415dca6571dfe76f
Clement Escoffier <clement.escoffier@gmail.com>
2019-10-30 16:15:44 +0100

    Use FixedHostPortGenericContainer

--- keycloak-authorization-quickstart/src/test/java/org.acme.keycloak.authorization/KeycloakServer.java
+++ keycloak-authorization-quickstart/src/test/java/org.acme.keycloak.authorization/KeycloakServer.java
@@ -1,11 +1,10 @@
 package org.acme.keycloak.authorization;
 
-import java.util.Arrays;
-
 import org.junit.jupiter.api.extension.AfterAllCallback;
 import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.testcontainers.containers.BindMode;
+import org.testcontainers.containers.FixedHostPortGenericContainer;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
 
@@ -15,14 +14,13 @@ public class KeycloakServer implements BeforeAllCallback, AfterAllCallback {
 
     @Override
     public void beforeAll(ExtensionContext extensionContext) throws Exception {
-        keycloak = new GenericContainer("quay.io/keycloak/keycloak")
-                .withExposedPorts(8080)
+        keycloak = new FixedHostPortGenericContainer("quay.io/keycloak/keycloak")
+                .withFixedExposedPort(8180, 8080)
                 .withEnv("KEYCLOAK_USER", "admin")
                 .withEnv("KEYCLOAK_PASSWORD", "admin")
                 .withEnv("KEYCLOAK_IMPORT", "/tmp/realm.json")
                 .withClasspathResourceMapping("quarkus-realm.json", "/tmp/realm.json", BindMode.READ_ONLY)
                 .waitingFor(Wait.forHttp("/auth"));
-        keycloak.setPortBindings(Arrays.asList("8180:8080"));
         keycloak.start();
     }
 
