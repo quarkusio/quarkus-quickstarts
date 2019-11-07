21bf8fcece04cc4800638e9736e28bc07736b4d5
elbaz michael <michaelelbaz@outlook.fr>
2019-11-07 16:55:26 +0100

    Fix wrong annotation import
    import javax.ws.rs.PathParam instead org.jboss.resteasy.annotations.jaxrs.PathParam

--- getting-started/src/main/java/org/acme/quickstart/GreetingResource.java
+++ getting-started/src/main/java/org/acme/quickstart/GreetingResource.java
@@ -4,21 +4,20 @@ package org.acme.quickstart;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-
-import org.jboss.resteasy.annotations.jaxrs.PathParam;
 
 @Path("/hello")
 public class GreetingResource {
 
     @Inject
     GreetingService service;
-
+    
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     @Path("/greeting/{name}")
-    public String greeting(@PathParam String name) {
+    public String greeting(@PathParam("name") String name) {
         return service.greeting(name);
     }
 
