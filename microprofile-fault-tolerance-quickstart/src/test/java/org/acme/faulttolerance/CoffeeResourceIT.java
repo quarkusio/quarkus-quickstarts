49dde9c3800379bd4060d2de063dda839e8bd3aa
Rostislav Svoboda <rsvoboda@redhat.com>
2019-11-18 14:03:18 +0100

    CoffeeResourceIT fix to extend CoffeeResourceTest

--- microprofile-fault-tolerance-quickstart/src/test/java/org/acme/faulttolerance/CoffeeResourceIT.java
+++ microprofile-fault-tolerance-quickstart/src/test/java/org/acme/faulttolerance/CoffeeResourceIT.java
@@ -1,35 +1,8 @@
 package org.acme.faulttolerance;
 
-import static io.restassured.RestAssured.get;
-import static org.hamcrest.CoreMatchers.hasItem;
-import static org.hamcrest.CoreMatchers.is;
-
 import io.quarkus.test.junit.NativeImageTest;
-import org.junit.jupiter.api.Test;
 
 @NativeImageTest
-public class CoffeeResourceIT {
-
-    @Test
-    public void testAvailability() {
-        get("/coffee/1/availability").then()
-                .statusCode(200).body(is("20"));
-        get("/coffee/1/availability").then()
-                .statusCode(200).body(is("20"));
-        get("/coffee/1/availability").then()
-                .statusCode(500).body(is("RuntimeException: Service failed."));
-        get("/coffee/1/availability").then()
-                .statusCode(500).body(is("RuntimeException: Service failed."));
-        get("/coffee/1/availability").then()
-                .statusCode(500).body(is("CircuitBreakerOpenException: getAvailability"));
-    }
-
-    @Test
-    public void testRecommendations() {
-        get("/coffee/2/recommendations").then()
-                .statusCode(200)
-                .body("id", hasItem(1),
-                        "countryOfOrigin", hasItem("Colombia"));
-    }
+public class CoffeeResourceIT extends CoffeeResourceTest {
 
 }
