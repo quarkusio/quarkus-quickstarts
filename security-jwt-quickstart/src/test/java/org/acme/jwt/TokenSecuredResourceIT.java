0a95db11e4c803b4052455ae2389a57d833b717d
Rostislav Svoboda <rsvoboda@redhat.com>
2019-11-19 09:06:31 +0100

    Use DisabledOnNativeImage annotation

--- security-jwt-quickstart/src/test/java/org/acme/jwt/TokenSecuredResourceIT.java
+++ security-jwt-quickstart/src/test/java/org/acme/jwt/TokenSecuredResourceIT.java
@@ -2,16 +2,8 @@ package org.acme.jwt;
 
 import io.quarkus.test.junit.NativeImageTest;
 
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
-/**
- * Native tests
- */
 @NativeImageTest
 public class TokenSecuredResourceIT extends TokenSecuredResourceTest {
-   @Test
-   @Override
-   @Disabled("Doesn't work in the native mode due to a subresource issue")
-   public void testLottoWinners() {
-   }
+
+   // Execute the same tests but in native mode.
 }
