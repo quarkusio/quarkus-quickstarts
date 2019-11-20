d2689b2761ca7223cdb94806fe59aa070138ee82
Clement Escoffier <clement.escoffier@gmail.com>
2019-11-20 08:07:23 +0100

    Set the version of keycloak to 7.0.1

--- security-openid-connect-web-authentication-quickstart/src/test/java/org/acme/quickstart/oidc/KeycloakServer.java
+++ security-openid-connect-web-authentication-quickstart/src/test/java/org/acme/quickstart/oidc/KeycloakServer.java
@@ -14,7 +14,7 @@ public class KeycloakServer implements BeforeAllCallback, AfterAllCallback {
 
     @Override
     public void beforeAll(ExtensionContext extensionContext) {
-        keycloak = new FixedHostPortGenericContainer("quay.io/keycloak/keycloak")
+        keycloak = new FixedHostPortGenericContainer("quay.io/keycloak/keycloak:7.0.1")
                 .withFixedExposedPort(8180, 8080)
                 .withEnv("KEYCLOAK_USER", "admin")
                 .withEnv("KEYCLOAK_PASSWORD", "admin")
