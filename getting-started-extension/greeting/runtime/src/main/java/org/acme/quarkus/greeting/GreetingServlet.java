194d6c4256375af934b247d0f6a882e2fbde5f0b
danielpetisme <daniel.petisme@gmail.com>
2019-12-13 23:30:27 +0100

    Extension content

---
+++ getting-started-extension/greeting/runtime/src/main/java/org/acme/quarkus/greeting/GreetingServlet.java
@@ -0,0 +1,17 @@
+package org.acme.quarkus.greeting;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet
+public class GreetingServlet extends HttpServlet { // <1>
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException { // <2>
+        resp.getWriter().write("Hello");
+    }
+}
