fc8392232a8ca5d1f013fd58ac64edc5aa9f3772
Musa Talluzi <musa.talluzi@gmail.com>
2019-12-28 16:09:23 -0500

    OptaPlanner quickstart

---
+++ optaplanner-quickstart/src/test/java/org/acme/optaplanner/rest/LessonResourceTest.java
@@ -0,0 +1,52 @@
+package org.acme.optaplanner.rest;
+
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+import java.util.List;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+import org.acme.optaplanner.domain.Lesson;
+import org.acme.optaplanner.domain.Timeslot;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+@QuarkusTest
+public class LessonResourceTest {
+
+    @Test
+    public void getAll() {
+        List<Lesson> lessonList = given()
+                .when().get("/lessons")
+                .then()
+                .statusCode(200)
+                .extract().body().jsonPath().getList(".", Lesson.class);
+        assertFalse(lessonList.isEmpty());
+        Lesson firstLesson = lessonList.get(0);
+        assertEquals("Biology", firstLesson.getSubject());
+        assertEquals("C. Darwin", firstLesson.getTeacher());
+        assertEquals("9th grade", firstLesson.getStudentGroup());
+    }
+
+    @Test
+    void addAndRemove() {
+        Lesson lesson = given()
+                .when()
+                .contentType(ContentType.JSON)
+                .body(new Lesson("Test subject", "Test teacher", "test studentGroup"))
+                .post("/lessons")
+                .then()
+                .statusCode(202)
+                .extract().as(Lesson.class);
+
+        given()
+                .when()
+                .delete("/lessons/{id}", lesson.getId())
+                .then()
+                .statusCode(200);
+    }
+
+}
