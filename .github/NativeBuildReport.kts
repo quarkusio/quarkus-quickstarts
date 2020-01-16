39163faa5040dfda05e93afd047ae76e39547989
Clement Escoffier <clement.escoffier@gmail.com>
2020-01-16 17:18:40 +0100

    Switch to use a single already created issue to report the build outcome

--- .github/NativeBuildReport.kts
+++ .github/NativeBuildReport.kts
@@ -1,7 +1,7 @@
 #!/usr/bin/env kscript
 
 @file:MavenRepository("jcenter","https://jcenter.bintray.com/")
-@file:MavenRepository("maven-central","https://repo1.maven.org/maven2/")
+@file:MavenRepository("maven-central","https://repo.maven.apache.org/maven2/")
 @file:DependsOn("org.kohsuke:github-api:1.101")
 
 
@@ -13,6 +13,8 @@ import java.util.concurrent.TimeUnit
 val token = args[0]; 
 val status = args[1];
 
+val ISSUE_NUMBER=6588
+val REPO = "quarkusio/quarkus"
 
 // Handle status. Possible values are success, failure, or cancelled.
 val succeed = status == "success";
@@ -21,14 +23,17 @@ if (status == "cancelled") {
     System.exit(0)
 }
 
-val ISSUE_TITLE = "[CI] - Quickstarts Native Build failed against Quarkus Master"
-val REPO = "quarkusio/quarkus-quickstarts"
-
 val github = GitHubBuilder().withOAuthToken(token).build()
-val repository = github.getRepository("quarkusio/quarkus")
-val issues = repository.getIssues(GHIssueState.ALL)
+val repository = github.getRepository(REPO)
 
-val issue = issues.firstOrNull { i -> i.getTitle() == ISSUE_TITLE}
+val issue = repository.getIssue(ISSUE_NUMBER)
+if (issue == null) {
+    println("Unable to find the issue ${ISSUE_NUMBER} in project ${REPO}")
+    System.exit(-1)
+} else {
+    println("Report issue found: ${issue.getTitle()} - ${issue.getHtmlUrl()}")
+    println("The issue is currently ${issue.getState()}")
+}
 
 val quickstartsCommit = getRepositoryCommit(".")
 val quarkusCommit = getRepositoryCommit("quarkus")
@@ -46,55 +51,30 @@ if (succeed) {
         issue.close()
         println("Comment added on issue ${issue.getHtmlUrl()} - ${comment.getHtmlUrl()}, the issue has also been closed")
     } else {
-        println("Nothing to do - the build passed and the issue is already closed or not created")
+        println("Nothing to do - the build passed and the issue is already closed")
     }
 } else  {
-    // Build failed
-    if (issue == null) {
-        // create a new issue
-        val newIssue = repository.createIssue(ISSUE_TITLE)
-            .body("""
-                The build of the  `development` branch of the quickstarts against quarkus `master` has failed.
-                This build verifies the native compilation and executes the native integration tests.
+    if (isOpen(issue)) {
+        val comment = issue.comment("""
+        The build is still failing with:
 
-                The build failed with:
+        * Quarkus commit: ${quarkusCommit}
+        * Quickstarts commit: ${quickstartsCommit}   
+        * Link to build: https://github.com/quarkusio/quarkus-quickstarts/actions
 
-                * Quarkus commit: ${quarkusCommit}
-                * Quickstarts commit: ${quickstartsCommit}     
-                * Link to build: https://github.com/quarkusio/quarkus-quickstarts/actions 
-                         
-                This issue will be closed when the build gets fixed. It will be re-opened if it fails again later.
-                Do not close the issue yourself (it will be re-opened automatically)
-                You can subscribe and monitor the status.
-
-            """.trimIndent())
-                .label("area/infra")
-                .create()
-        println("New issue created: ${newIssue.getTitle()} - ${newIssue.getHtmlUrl()}")    
+    """.trimIndent())
+        println("Comment added on issue ${issue.getHtmlUrl()} - ${comment.getHtmlUrl()}")
     } else {
-        if (isOpen(issue)) {
-            val comment = issue.comment("""
-            Build still failing with:
+        issue.reopen()
+        val comment = issue.comment("""
+        Unfortunately, the build failed:
 
-            * Quarkus commit: ${quarkusCommit}
-            * Quickstarts commit: ${quickstartsCommit}   
-            * Link to build: https://github.com/quarkusio/quarkus-quickstarts/actions
+        * Quarkus commit: ${quarkusCommit}
+        * Quickstarts commit: ${quickstartsCommit}   
+        * Link to build: https://github.com/quarkusio/quarkus-quickstarts/actions
 
-        """.trimIndent())
-            println("Comment added on issue ${issue.getHtmlUrl()} - ${comment.getHtmlUrl()}")
-        } else {
-            issue.reopen()
-            val comment = issue.comment("""
-            Unfortunately, the build fails again:
-
-            * Quarkus commit: ${quarkusCommit}
-            * Quickstarts commit: ${quickstartsCommit}   
-            * Link to build: https://github.com/quarkusio/quarkus-quickstarts/actions
-
-        """.trimIndent())
-            println("Comment added on issue ${issue.getHtmlUrl()} - ${comment.getHtmlUrl()}, the issue has been re-opened.")
-
-        }
+    """.trimIndent())
+        println("Comment added on issue ${issue.getHtmlUrl()} - ${comment.getHtmlUrl()}, the issue has been re-opened.")
 
     }
 }
@@ -113,7 +93,7 @@ fun String.runCommand(workingDir: String): String {
                 .redirectError(ProcessBuilder.Redirect.PIPE)
                 .start()
 
-        proc.waitFor(60, TimeUnit.MINUTES)
+        proc.waitFor(1, TimeUnit.MINUTES)
         return proc.inputStream.bufferedReader().readText()
     } catch(e: IOException) {
         e.printStackTrace()
