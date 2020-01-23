9a1459774b22f77ae0084233c5a9c023b8ef586a
Stuart Douglas <stuart.w.douglas@gmail.com>
2020-01-24 09:58:32 +1100

    Fix Kafka Quickstart

---
+++ kafka-quickstart/src/test/java/org/acme/quarkus/sample/KafkaResource.java
@@ -0,0 +1,27 @@
+package org.acme.quarkus.sample;
+
+import java.util.Collections;
+import java.util.Map;
+
+import org.testcontainers.containers.KafkaContainer;
+
+import io.quarkus.test.common.QuarkusTestResourceLifecycleManager;
+
+public class KafkaResource implements QuarkusTestResourceLifecycleManager {
+
+    private final KafkaContainer KAFKA = new KafkaContainer();
+
+    @Override
+    public Map<String, String> start() {
+        KafkaContainer KAFKA = new KafkaContainer();
+        KAFKA.start();
+        System.setProperty("kafka.bootstrap.servers", KAFKA.getBootstrapServers());
+        return Collections.emptyMap();
+    }
+
+    @Override
+    public void stop() {
+        System.clearProperty("kafka.bootstrap.servers");
+        KAFKA.close();
+    }
+}
