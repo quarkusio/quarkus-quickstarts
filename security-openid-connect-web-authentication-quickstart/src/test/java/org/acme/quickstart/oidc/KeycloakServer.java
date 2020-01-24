3d31324f352d6b30d5612dd5c6b01a2470e5f555
Stuart Douglas <stuart.w.douglas@gmail.com>
2020-01-24 11:47:33 +1100

    Fix keycloak issue

--- security-openid-connect-web-authentication-quickstart/src/test/java/org/acme/quickstart/oidc/KeycloakServer.java
+++ security-openid-connect-web-authentication-quickstart/src/test/java/org/acme/quickstart/oidc/KeycloakServer.java
@@ -1,19 +1,22 @@
 package org.acme.quickstart.oidc;
 
-import org.junit.jupiter.api.extension.AfterAllCallback;
-import org.junit.jupiter.api.extension.BeforeAllCallback;
-import org.junit.jupiter.api.extension.ExtensionContext;
+import java.util.Collections;
+import java.util.Map;
+
 import org.testcontainers.containers.BindMode;
 import org.testcontainers.containers.FixedHostPortGenericContainer;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
 
-public class KeycloakServer implements BeforeAllCallback, AfterAllCallback {
+import io.quarkus.test.common.QuarkusTestResourceLifecycleManager;
+
+public class KeycloakServer implements QuarkusTestResourceLifecycleManager {
 
     private GenericContainer keycloak;
 
     @Override
-    public void beforeAll(ExtensionContext extensionContext) {
+    public Map<String, String> start() {
+
         keycloak = new FixedHostPortGenericContainer("quay.io/keycloak/keycloak:7.0.1")
                 .withFixedExposedPort(8180, 8080)
                 .withEnv("KEYCLOAK_USER", "admin")
@@ -22,10 +25,11 @@ public class KeycloakServer implements BeforeAllCallback, AfterAllCallback {
                 .withClasspathResourceMapping("quarkus-realm.json", "/tmp/realm.json", BindMode.READ_ONLY)
                 .waitingFor(Wait.forHttp("/auth"));
         keycloak.start();
+        return Collections.emptyMap();
     }
 
     @Override
-    public void afterAll(ExtensionContext extensionContext) {
+    public void stop() {
         keycloak.stop();
     }
 }
