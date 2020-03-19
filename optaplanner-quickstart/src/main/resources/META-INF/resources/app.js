30784f0e0f7384f717dd362b74d682d0161c1e54
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2020-03-19 11:48:29 +0100

    OptaPlanner quickstart: UI feedback

--- optaplanner-quickstart/src/main/resources/META-INF/resources/app.js
+++ optaplanner-quickstart/src/main/resources/META-INF/resources/app.js
@@ -20,14 +20,11 @@ function refreshTimeTable() {
         headerRowByRoom.append($("<th>Timeslot</th>"));
         $.each(timeTable.roomList, (index, room) => {
             headerRowByRoom
-            .append($("<th>")
+            .append($("<th/>")
                 .append($("<span/>").text(room.name))
-                .append($(`
-                  <button type="button" class="ml-2 mb-1 btn btn-light btn-sm p-1">
-                    <small class="fas fa-trash"></small>
-                  </button>`
-                ).click(() => deleteRoom(room)))
-            .append("</th>"));
+                .append($(`<button type="button" class="ml-2 mb-1 btn btn-light btn-sm p-1"/>`)
+                        .append($(`<small class="fas fa-trash"/>`)
+                        ).click(() => deleteRoom(room))));
         });
         const theadByTeacher = $("<thead>").appendTo(timeTableByTeacher);
         const headerRowByTeacher = $("<tr>").appendTo(theadByTeacher);
@@ -35,9 +32,8 @@ function refreshTimeTable() {
         const teacherList = [...new Set(timeTable.lessonList.map(lesson => lesson.teacher))];
         $.each(teacherList, (index, teacher) => {
             headerRowByTeacher
-            .append($("<th>")
-                .append($("<span />").text(teacher))
-            .append("</th>"));
+            .append($("<th/>")
+                .append($("<span/>").text(teacher)));
         });
         const theadByStudentGroup = $("<thead>").appendTo(timeTableByStudentGroup);
         const headerRowByStudentGroup = $("<tr>").appendTo(theadByStudentGroup);
@@ -45,9 +41,8 @@ function refreshTimeTable() {
         const studentGroupList = [...new Set(timeTable.lessonList.map(lesson => lesson.studentGroup))];
         $.each(studentGroupList, (index, studentGroup) => {
             headerRowByStudentGroup
-            .append($("<th>")
-                .append($("<span />").text(studentGroup))
-            .append("</th>"));
+            .append($("<th/>")
+                .append($("<span/>").text(studentGroup)));
         });
 
         const tbodyByRoom = $("<tbody>").appendTo(timeTableByRoom);
@@ -56,43 +51,38 @@ function refreshTimeTable() {
         $.each(timeTable.timeslotList, (index, timeslot) => {
             const rowByRoom = $("<tr>").appendTo(tbodyByRoom);
             rowByRoom
-            .append($(`<th class="align-middle">`)
+            .append($(`<th class="align-middle"/>`)
                 .append($("<span/>").text(`
                     ${timeslot.dayOfWeek.charAt(0) + timeslot.dayOfWeek.slice(1).toLowerCase()}
                     ${moment(timeslot.startTime, "HH:mm:ss").format("HH:mm")}
                     -
                     ${moment(timeslot.endTime, "HH:mm:ss").format("HH:mm")}
                 `)
-                .append($(`
-                    <button type="button" class="ml-2 mb-1 btn btn-light btn-sm p-1">
-                        <small class="fas fa-trash"></small>
-                    </button>
-                `).click(() => deleteTimeslot(timeslot)))
-            .append("</th>")));
+                .append($(`<button type="button" class="ml-2 mb-1 btn btn-light btn-sm p-1"/>`)
+                        .append($(`<small class="fas fa-trash"/>`)
+                        ).click(() => deleteTimeslot(timeslot)))));
 
             const rowByTeacher = $("<tr>").appendTo(tbodyByTeacher);
             rowByTeacher
-            .append($(`<th class="align-middle">`)
+            .append($(`<th class="align-middle"/>`)
                 .append($("<span/>").text(`
                     ${timeslot.dayOfWeek.charAt(0) + timeslot.dayOfWeek.slice(1).toLowerCase()}
                     ${moment(timeslot.startTime, "HH:mm:ss").format("HH:mm")}
                     -
                     ${moment(timeslot.endTime, "HH:mm:ss").format("HH:mm")}
-                `)
-            .append("</th>")));
+                `)));
             $.each(timeTable.roomList, (index, room) => {
                 rowByRoom.append($("<td/>").prop("id", `timeslot${timeslot.id}room${room.id}`));
             });
             const rowByStudentGroup = $("<tr>").appendTo(tbodyByStudentGroup);
             rowByStudentGroup
-            .append($(`<th class="align-middle">`)
+            .append($(`<th class="align-middle"/>`)
                 .append($("<span/>").text(`
                     ${timeslot.dayOfWeek.charAt(0) + timeslot.dayOfWeek.slice(1).toLowerCase()}
                     ${moment(timeslot.startTime, "HH:mm:ss").format("HH:mm")}
                     -
                     ${moment(timeslot.endTime, "HH:mm:ss").format("HH:mm")}
-                `))
-            .append("</th>"));
+                `)));
 
             $.each(teacherList, (index, teacher) => {
                 rowByTeacher.append($("<td/>").prop("id", `timeslot${timeslot.id}teacher${convertToId(teacher)}`));
@@ -105,22 +95,18 @@ function refreshTimeTable() {
 
         $.each(timeTable.lessonList, (index, lesson) => {
             const color = pickColor(lesson.subject);
-            const lessonElementWithoutDelete = $(
-            `<div class="card lesson" style="background-color: ${color}">`)
-                .append($(`<div class="card-body p-2">`)
-                    .append($(`<h5 class="card-title mb-1" />`).text(lesson.subject))
-                    .append($(`<p class="card-text text-muted ml-2 mb-1" />`).text(`by ${lesson.teacher}`))
-                    .append($(`<small class="ml-2 mt-1 card-text text-muted align-bottom float-right" />`).text(lesson.id))
-                    .append($(`<p class="card-text ml-2" />`).text(lesson.studentGroup))
-                .append("</div>"))
-            .append(`</div>`);
+            const lessonElementWithoutDelete = $(`<div class="card lesson" style="background-color: ${color}"/>`)
+                    .append($(`<div class="card-body p-2"/>`)
+                            .append($(`<h5 class="card-title mb-1"/>`).text(lesson.subject))
+                            .append($(`<p class="card-text ml-2 mb-1"/>`)
+                                    .append($(`<em/>`).text(`by ${lesson.teacher}`)))
+                            .append($(`<small class="ml-2 mt-1 card-text text-muted align-bottom float-right"/>`).text(lesson.id))
+                            .append($(`<p class="card-text ml-2"/>`).text(lesson.studentGroup)));
             const lessonElement = lessonElementWithoutDelete.clone();
             lessonElement.find(".card-body").prepend(
-                $(`
-                    <button type="button" class="ml-2 btn btn-light btn-sm p-1 float-right">
-                        <small class="fas fa-trash"></small>
-                    </button>
-                `).click(() => deleteLesson(lesson))
+                $(`<button type="button" class="ml-2 btn btn-light btn-sm p-1 float-right"/>`)
+                        .append($(`<small class="fas fa-trash"/>`)
+                        ).click(() => deleteLesson(lesson))
             );
             if (lesson.timeslot == null || lesson.room == null) {
                 unassignedLessons.append(lessonElement);
@@ -244,22 +230,19 @@ function deleteRoom(room) {
 function showError(title, xhr) {
     const serverErrorMessage = !xhr.responseJSON ? `${xhr.status}: ${xhr.statusText}` : xhr.responseJSON.message;
     console.error(title + "\n" + serverErrorMessage);
-    const notification = $(`
-    <div class="toast" role="alert" role="alert" aria-live="assertive" aria-atomic="true" style="min-width: 30rem">
-        <div class="toast-header bg-danger">
-            <strong class="mr-auto text-dark">Error</strong>
-            <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-        </div>
-        <div class="toast-body">
-      `)
-        .append($(`<p/>`).text(title))
-        .append($(`<pre>`)
-            .append($(`<code />`).text(serverErrorMessage))
-        .append(`</pre>
-        </div>
-    </div>`));
+    const notification = $(`<div class="toast" role="alert" role="alert" aria-live="assertive" aria-atomic="true" style="min-width: 30rem"/>`)
+            .append($(`<div class="toast-header bg-danger">
+                            <strong class="mr-auto text-dark">Error</strong>
+                            <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>`))
+            .append($(`<div class="toast-body"/>`)
+                    .append($(`<p/>`).text(title))
+                    .append($(`<pre/>`)
+                            .append($(`<code/>`).text(serverErrorMessage))
+                    )
+            );
     $("#notificationPanel").append(notification);
     notification.toast({delay: 30000});
     notification.toast('show');
