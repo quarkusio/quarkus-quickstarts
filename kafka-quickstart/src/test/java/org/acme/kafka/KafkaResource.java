26ba14700c11bdf657a9df0212d5e2264b5dc03c
Guillaume Smet <guillaume.smet@gmail.com>
2020-04-02 15:48:53 +0200

    Fix Kafka test resource
    Fixes #517

--- kafka-quickstart/src/test/java/org/acme/kafka/KafkaResource.java
+++ kafka-quickstart/src/test/java/org/acme/kafka/KafkaResource.java
@@ -9,19 +9,21 @@ import io.quarkus.test.common.QuarkusTestResourceLifecycleManager;
 
 public class KafkaResource implements QuarkusTestResourceLifecycleManager {
 
-    private final KafkaContainer KAFKA = new KafkaContainer();
+    private KafkaContainer kafka;
 
     @Override
     public Map<String, String> start() {
-        KafkaContainer KAFKA = new KafkaContainer();
-        KAFKA.start();
-        System.setProperty("kafka.bootstrap.servers", KAFKA.getBootstrapServers());
+        kafka = new KafkaContainer();
+        kafka.start();
+        System.setProperty("kafka.bootstrap.servers", kafka.getBootstrapServers());
         return Collections.emptyMap();
     }
 
     @Override
     public void stop() {
         System.clearProperty("kafka.bootstrap.servers");
-        KAFKA.close();
+        if (kafka != null) {
+            kafka.close();
+        }
     }
 }
