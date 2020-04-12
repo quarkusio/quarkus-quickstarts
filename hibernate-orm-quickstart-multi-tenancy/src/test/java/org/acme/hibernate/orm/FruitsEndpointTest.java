8aaeaf59fc74eedc1dd84e687cbbe8113fd32af0
Michael Schnell <michael@fuin.org>
2020-04-12 11:06:11 +0200

    Test both schemas

--- hibernate-orm-quickstart-multi-tenancy/src/test/java/org/acme/hibernate/orm/FruitsEndpointTest.java
+++ hibernate-orm-quickstart-multi-tenancy/src/test/java/org/acme/hibernate/orm/FruitsEndpointTest.java
@@ -12,7 +12,7 @@ import io.quarkus.test.junit.QuarkusTest;
 public class FruitsEndpointTest {
 
     @Test
-    public void testListAllFruits() {
+    public void testListAllFruitsBaseSchema() {
         //List all, should have all 3 fruits the database has initially:
         given()
                 .when().get("/fruits")
@@ -58,6 +58,55 @@ public class FruitsEndpointTest {
                         containsString("Apple"),
                         containsString("Banana"),
                         containsString("Pear"));
+    }
+
+    @Test
+    public void testListAllFruitsMyCompanySchema() {
+        //List all, should have all 3 fruits the database has initially:
+        given()
+                .when().get("/mycompany/fruits")
+                .then()
+                .statusCode(200)
+                .body(
+                        containsString("Avocado"),
+                        containsString("Apricots"),
+                        containsString("Blackberries"));
+
+        //Delete the Cherry:
+        given()
+                .when().delete("/mycompany/fruits/1")
+                .then()
+                .statusCode(204);
+
+        //List all, Avocado should be missing now:
+        given()
+                .when().get("/mycompany/fruits")
+                .then()
+                .statusCode(200)
+                .body(
+                        not(containsString("Avocado")),
+                        containsString("Apricots"),
+                        containsString("Blackberries"));
+
+        //Create the Clementine:
+        given()
+                .when()
+                .body("{\"name\" : \"Clementine\"}")
+                .contentType("application/json")
+                .post("/mycompany/fruits")
+                .then()
+                .statusCode(201);
+
+        //List all, Avocado should be missing and Clementine added now:
+        given()
+                .when().get("/mycompany/fruits")
+                .then()
+                .statusCode(200)
+                .body(
+                        not(containsString("Avocado")),
+                        containsString("Apricots"),
+                        containsString("Blackberries"),
+                        containsString("Clementine"));
     }
 
 }
