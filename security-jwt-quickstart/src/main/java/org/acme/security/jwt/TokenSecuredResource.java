ffe0505445e165c872c09a63eebf23c0a477a587
Sergey Beryozkin <sberyozkin@gmail.com>
2020-04-17 11:02:38 +0100

    Anonymous user is now represented by Principal with an empty name

--- security-jwt-quickstart/src/main/java/org/acme/security/jwt/TokenSecuredResource.java
+++ security-jwt-quickstart/src/main/java/org/acme/security/jwt/TokenSecuredResource.java
@@ -42,7 +42,7 @@ public class TokenSecuredResource {
     @Produces(MediaType.TEXT_PLAIN)
     public String hello(@Context SecurityContext ctx) {
         Principal caller = ctx.getUserPrincipal();
-        String name = caller == null ? "anonymous" : caller.getName();
+        String name = caller.getName().isEmpty() ? "anonymous" : caller.getName();
         boolean hasJWT = jwt.getClaimNames() != null;
         String helloReply = String.format("hello + %s, isSecure: %s, authScheme: %s, hasJWT: %s", name, ctx.isSecure(),
                 ctx.getAuthenticationScheme(), hasJWT);
@@ -55,7 +55,7 @@ public class TokenSecuredResource {
     @Produces(MediaType.TEXT_PLAIN)
     public String helloRolesAllowed(@Context SecurityContext ctx) {
         Principal caller = ctx.getUserPrincipal();
-        String name = caller == null ? "anonymous" : caller.getName();
+        String name = caller.getName().isEmpty() ? "anonymous" : caller.getName();
         boolean hasJWT = jwt.getClaimNames() != null;
         String helloReply = String.format("hello + %s, isSecure: %s, authScheme: %s, hasJWT: %s", name, ctx.isSecure(),
                 ctx.getAuthenticationScheme(), hasJWT);
@@ -68,7 +68,7 @@ public class TokenSecuredResource {
     @Produces(MediaType.TEXT_PLAIN)
     public String helloShouldDeny(@Context SecurityContext ctx) {
         Principal caller = ctx.getUserPrincipal();
-        String name = caller == null ? "anonymous" : caller.getName();
+        String name = caller.getName().isEmpty() ? "anonymous" : caller.getName();
         return "hello + " + name;
     }
 
