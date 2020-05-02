b3abd2df02c3d52e18cdcd87b656acf2ce96de9a
Paolo Carta <ing.pcarta@gmail.com>
2020-05-02 18:25:56 +0200

    removing wildcard imports

--- infinispan-embedded-quickstart/src/main/java/org/acme/tokenmanager/config/DefaultCache.java
+++ infinispan-embedded-quickstart/src/main/java/org/acme/tokenmanager/config/DefaultCache.java
@@ -1,7 +1,11 @@
 package org.acme.tokenmanager.config;
 
 import javax.inject.Qualifier;
-import java.lang.annotation.*;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Qualifier
 @Target({ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD})
--- infinispan-embedded-quickstart/src/main/java/org/acme/tokenmanager/controllers/CacheEntryResource.java
+++ infinispan-embedded-quickstart/src/main/java/org/acme/tokenmanager/controllers/CacheEntryResource.java
@@ -9,7 +9,12 @@ import org.jboss.logging.Logger;
 
 import javax.inject.Inject;
 import javax.validation.Valid;
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
