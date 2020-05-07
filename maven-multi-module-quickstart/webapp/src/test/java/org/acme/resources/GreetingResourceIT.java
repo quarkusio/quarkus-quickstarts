4a7907b31ad0236bed495d344c15aa910269b159
Willem Jan Glerum <willem.jan.glerum@lunatech.nl>
2020-05-07 18:29:50 +0200

    Fix tests

---
+++ maven-multi-module-quickstart/webapp/src/test/java/org/acme/resources/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package org.acme.resources;
+
+import io.quarkus.test.junit.NativeImageTest;
+import org.acme.webapp.GreetingResourceTest;
+
+@NativeImageTest
+public class GreetingResourceIT extends GreetingResourceTest {
+}
