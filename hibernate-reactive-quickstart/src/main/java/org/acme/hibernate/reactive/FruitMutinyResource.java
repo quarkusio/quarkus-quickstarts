f2abc7d9718af320846ab4143ee428bc9b6fd5c0
Davide D'Alto <davide@hibernate.org>
2020-07-02 12:33:07 +0100

    Use Mutiny event-based API in the Hibernate Reactive quickstart
      * Inject the Session directly, unwrap from Uni
      * Additional tests for update and not found entities
      * Return Multi<Fruit> instead of Uni<Fruit[]>
      * Add example on how to map exceptions in the HTTP response

--- hibernate-reactive-quickstart/src/main/java/org/acme/hibernate/reactive/FruitMutinyResource.java
+++ hibernate-reactive-quickstart/src/main/java/org/acme/hibernate/reactive/FruitMutinyResource.java
@@ -1,7 +1,11 @@
 package org.acme.hibernate.reactive;
 
+import java.util.List;
+import java.util.function.Function;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import javax.json.Json;
+import javax.json.JsonObjectBuilder;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -11,11 +15,15 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
 
 import org.hibernate.reactive.mutiny.Mutiny;
 
+import org.jboss.logging.Logger;
 import org.jboss.resteasy.annotations.jaxrs.PathParam;
 
+import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.Uni;
 
 @Path("fruits")
@@ -23,67 +31,118 @@ import io.smallrye.mutiny.Uni;
 @Produces("application/json")
 @Consumes("application/json")
 public class FruitMutinyResource {
+    private static final Logger LOGGER = Logger.getLogger(FruitMutinyResource.class.getName());
 
     @Inject
-    Uni<Mutiny.Session> mutinySession;
+    Mutiny.Session mutinySession;
 
     @GET
-    public Uni<Fruit[]> get() {
-        return mutinySession.flatMap( session -> session.createNamedQuery( "Fruits.findAll", Fruit.class ).getResultList() )
-                .map( fruits -> fruits.toArray( new Fruit[fruits.size()] ) );
+    public Multi<Fruit> get() {
+        return mutinySession
+                .createNamedQuery( "Fruits.findAll", Fruit.class ).getResults();
     }
 
     @GET
     @Path("{id}")
     public Uni<Fruit> getSingle(@PathParam Integer id) {
-        return mutinySession
-                .flatMap( session -> session.find( Fruit.class, id ) );
+        return mutinySession.find(Fruit.class, id);
     }
 
     @POST
     public Uni<Response> create(Fruit fruit) {
-        if (fruit.getId() != null) {
+        if (fruit == null || fruit.getId() != null) {
             throw new WebApplicationException("Id was invalidly set on request.", 422);
         }
+
         return mutinySession
-                .flatMap( session -> session.persist( fruit ) )
-                .flatMap( session -> session.flush() )
-                .map( ignore -> Response.ok( fruit ).status( 201 ).build() );
+                .persist(fruit)
+                .onItem().produceUni(session -> mutinySession.flush())
+                .onItem().apply(ignore -> Response.ok(fruit).status(201).build());
     }
 
     @PUT
     @Path("{id}")
-    public Uni<Fruit>  update(@PathParam Integer id, Fruit fruit) {
-        if (fruit.getName() == null) {
-            throw new WebApplicationException("Fruit Name was not set on request.", 422);
+    public Uni<Response> update(@PathParam Integer id, Fruit fruit) {
+        if (fruit == null || fruit.getName() == null) {
+            throw new WebApplicationException("Fruit name was not set on request.", 422);
         }
 
+        // Update function (never returns null)
+        Function<Fruit, Uni<Response>> update = entity -> {
+            entity.setName(fruit.getName());
+            return mutinySession.flush()
+                    .onItem().apply(ignore -> Response.ok(entity).build());
+        };
+
         return mutinySession
-                .flatMap( session -> session.find( Fruit.class, id )
-                        .flatMap( entity -> {
-                            if (entity == null) {
-                                throw new WebApplicationException("Fruit with id of " + id + " does not exist.", 404);
-                            }
-                            entity.setName( fruit.getName() );
-                            return session.flush()
-                                    .map( ignore -> entity );
-                        } )
-                );
+                .find( Fruit.class, id )
+                      // If entity exists then
+                    .onItem().ifNotNull()
+                        .produceUni(update)
+                    // else
+                    .onItem().ifNull()
+                        .continueWith(Response.ok().status(404).build());
     }
 
     @DELETE
     @Path("{id}")
     public Uni<Response> delete(@PathParam Integer id) {
+        // Delete function (never returns null)
+        Function<Fruit, Uni<Response>> delete = entity -> mutinySession.remove(entity)
+                .onItem().produceUni(ignore -> mutinySession.flush())
+                .onItem().apply(ignore -> Response.ok().status(204).build());
+
         return mutinySession
-                .flatMap( session -> session.find( Fruit.class, id )
-                        .flatMap( entity -> {
-                            if ( entity == null ) {
-                                throw new WebApplicationException( "Fruit with id of " + id + " does not exist.", 404 );
-                            }
-                            return session.remove( entity );
-                        } )
-                )
-                .flatMap( session -> session.flush() )
-                .map( ignore -> Response.ok().status( 204 ).build() );
+                .find( Fruit.class, id )
+                    // If entity exists then
+                    .onItem().ifNotNull()
+                        .produceUni(delete)
+                    // else
+                    .onItem().ifNull()
+                        .continueWith(Response.ok().status(404).build());
+    }
+
+    /**
+     * Create a HTTP response from an exception.
+     *
+     * Response Example:
+     *
+     * <pre>
+     * HTTP/1.1 422 Unprocessable Entity
+     * Content-Length: 111
+     * Content-Type: application/json
+     *
+     * {
+     *     "code": 422,
+     *     "error": "Fruit name was not set on request.",
+     *     "exceptionType": "javax.ws.rs.WebApplicationException"
+     * }
+     * </pre>
+     */
+    @Provider
+    public static class ErrorMapper implements ExceptionMapper<Exception> {
+
+        @Override
+        public Response toResponse(Exception exception) {
+            LOGGER.error("Failed to handle request", exception);
+
+            int code = 500;
+            if (exception instanceof WebApplicationException) {
+                code = ((WebApplicationException) exception).getResponse().getStatus();
+            }
+
+            JsonObjectBuilder entityBuilder = Json.createObjectBuilder()
+                    .add("exceptionType", exception.getClass().getName())
+                    .add("code", code);
+
+            if (exception.getMessage() != null) {
+                entityBuilder.add("error", exception.getMessage());
+            }
+
+            return Response.status(code)
+                    .entity(entityBuilder.build())
+                    .build();
+        }
+
     }
 }
