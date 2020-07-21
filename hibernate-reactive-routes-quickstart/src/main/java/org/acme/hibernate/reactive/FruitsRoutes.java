3421fbe721239058c59f44c6529a0c1d2a6b0877
Martin Kouba <mkouba@redhat.com>
2020-07-21 15:03:36 +0200

    Hibernate reactive routes quickstart improvements
    - based on the latest reactive routes API improvements

--- hibernate-reactive-routes-quickstart/src/main/java/org/acme/hibernate/reactive/FruitsRoutes.java
+++ hibernate-reactive-routes-quickstart/src/main/java/org/acme/hibernate/reactive/FruitsRoutes.java
@@ -1,123 +1,112 @@
 package org.acme.hibernate.reactive;
 
-import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
-
-import org.hibernate.reactive.mutiny.Mutiny;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import io.quarkus.vertx.web.Route;
-import io.quarkus.vertx.web.RouteBase;
-import io.smallrye.mutiny.Multi;
-import io.smallrye.mutiny.Uni;
-import io.vertx.core.http.HttpServerResponse;
-import io.vertx.ext.web.RoutingContext;
-
+import static io.quarkus.vertx.web.Route.HandlerType.FAILURE;
 import static io.vertx.core.http.HttpMethod.DELETE;
 import static io.vertx.core.http.HttpMethod.GET;
 import static io.vertx.core.http.HttpMethod.POST;
 import static io.vertx.core.http.HttpMethod.PUT;
 
+import java.util.NoSuchElementException;
+
+import javax.inject.Inject;
+
+import org.hibernate.reactive.mutiny.Mutiny;
+import org.jboss.logging.Logger;
+
+import io.quarkus.vertx.web.Body;
+import io.quarkus.vertx.web.Param;
+import io.quarkus.vertx.web.ReactiveRoutes;
+import io.quarkus.vertx.web.Route;
+import io.quarkus.vertx.web.RouteBase;
+import io.smallrye.mutiny.Multi;
+import io.smallrye.mutiny.Uni;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.RoutingContext;
+
 /**
  * An example using Vert.x Web, Hibernate Reactive and Mutiny.
  */
-@ApplicationScoped
 @RouteBase(path = "/fruits", produces = "application/json")
 public class FruitsRoutes {
 
-    @Inject
-    ObjectMapper mapper;
+    private static final Logger LOGGER = Logger.getLogger(FruitsRoutes.class.getName());
 
     @Inject
     Mutiny.Session session;
 
-    /**
-     * Convert an object into a JSON string.
-     */
-    private String toJSON(Object object) {
-        try {
-            return mapper.writeValueAsString(object);
-        } catch (JsonProcessingException e) {
-            throw new IllegalArgumentException(e);
-        }
-    }
-
     @Route(methods = GET, path = "/")
-    public Multi<Fruit> getAll(RoutingContext rc) throws Exception {
-        return session.createNamedQuery(Fruit.FIND_ALL, Fruit.class).getResults();
+    public Multi<Fruit> getAll() throws Exception {
+        return ReactiveRoutes.asJsonArray(session.createNamedQuery(Fruit.FIND_ALL, Fruit.class).getResults());
     }
 
     @Route(methods = GET, path = "/:id")
-    public Uni<Fruit> getSingle(RoutingContext rc) {
-        final Integer id = Integer.valueOf(rc.request().getParam("id"));
-
-        return session.find(Fruit.class, id);
+    public Uni<Fruit> getSingle(@Param String id) {
+        return session.find(Fruit.class, Integer.valueOf(id));
     }
 
     @Route(methods = POST, path = "/")
-    public Uni<HttpServerResponse> create(RoutingContext rc) {
-        final String name = rc.getBodyAsJson().getString("name");
-        final Fruit entity = new Fruit(name);
-
-        return session.persist(entity)
-                        .onItem().produceUni(s -> session.flush())
-                        .onItem().apply(ignore -> httpResponse(rc, 201, entity));
+    public Uni<Fruit> create(@Body Fruit fruit, HttpServerResponse response) {
+        if (fruit == null || fruit.getId() != null) {
+            return Uni.createFrom().failure(new IllegalArgumentException("Fruit id invalidly set on request."));
+        }
+        return session.persist(fruit)
+                .onItem().transformToUni(session -> session.flush())
+                .onItem().transform(ignore -> {
+                    response.setStatusCode(201);
+                    return fruit;
+                });
     }
 
     @Route(methods = PUT, path = "/:id")
-    public Uni<HttpServerResponse> update(RoutingContext rc) {
-        final Integer id = Integer.valueOf(rc.request().getParam("id"));
-        final String name = rc.getBodyAsJson().getString("name");
-
-        return session.find(Fruit.class, id)
-                // if entity exists
-                .onItem().ifNotNull()
-                        .produceUni(entity -> {
-                                // Update the entity
-                                entity.setName(name);
-                                return session.flush()
-                                        .onItem().apply(ignore -> httpResponse(rc, 200, entity));
-                        })
-                // if entity doesn't exist
-                .onItem().ifNull()
-                        // Not found response code
-                        .continueWith(httpResponse(rc, 404));
+    public Uni<Fruit> update(@Body Fruit fruit, @Param String id) {
+        if (fruit == null || fruit.getName() == null) {
+            return Uni.createFrom().failure(new IllegalArgumentException("Fruit name was not set on request."));
+        }
+        return session.find(Fruit.class, Integer.valueOf(id))
+                // If entity exists then update
+                .onItem().ifNotNull().transformToUni(entity -> {
+                    entity.setName(fruit.getName());
+                    return session.flush()
+                            .onItem().transform(ignore -> entity);
+                })
+                // else
+                .onItem().ifNull().fail();
     }
 
     @Route(methods = DELETE, path = "/:id")
-    public Uni<HttpServerResponse>  delete(RoutingContext rc) {
-        final Integer id = Integer.valueOf(rc.request().getParam("id"));
-
-        return session.find(Fruit.class, id)
-                // if entity exists
-                .onItem().ifNotNull()
-                        .produceUni(entity ->
-                                // Remove the entity
-                                session.remove(entity)
-                                        .onItem().produceUni(ignore -> session.flush())
-                                        .onItem().apply(ignore -> httpResponse(rc, 204))
-                )
-                // if entity doesn't exist
-                .onItem().ifNull()
-                        // Not found response code
-                        .continueWith(httpResponse(rc, 404));
+    public Uni<Fruit> delete(@Param String id, HttpServerResponse response) {
+        return session.find(Fruit.class, Integer.valueOf(id))
+                // If entity exists then delete
+                .onItem().ifNotNull().transformToUni(entity -> session.remove(entity)
+                        .onItem().transformToUni(ignore -> session.flush())
+                        .onItem().transform(ignore -> {
+                            response.setStatusCode(204).end();
+                            return entity;
+                        }))
+                // else
+                .onItem().ifNull().fail();
     }
 
-    protected static HttpServerResponse httpResponse(RoutingContext rc, int statusCode) {
-        return rc.response()
-                .setStatusCode(statusCode)
-                .putHeader("Content-Type", "application/json");
+    @Route(path = "/*", type = FAILURE)
+    public void error(RoutingContext context) {
+        Throwable t = context.failure();
+        if (t != null) {
+            LOGGER.error("Failed to handle request", t);
+            int status = context.statusCode();
+            String chunk = "";
+            if (t instanceof NoSuchElementException) {
+                status = 404;
+            } else if (t instanceof IllegalArgumentException) {
+                status = 422;
+                chunk = new JsonObject().put("code", status)
+                        .put("exceptionType", t.getClass().getName()).put("error", t.getMessage()).encode();
+            }
+            context.response().setStatusCode(status).end(chunk);
+        } else {
+            // Continue with the default error handler
+            context.next();
+        }
     }
 
-    /**
-     * Generate the response with the specified status code and add the object to the body as JSON.
-     */
-    protected HttpServerResponse httpResponse(RoutingContext rc, int statusCode, Object object) {
-        String asJson = toJSON(object);
-        return httpResponse(rc, statusCode)
-                // Content-Length header is required when using .write
-                .putHeader("Content-Length", Long.toString(asJson.length()))
-                .write(asJson);
-    }
 }
