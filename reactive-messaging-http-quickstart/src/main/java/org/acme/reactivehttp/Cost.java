933fb7d2403569a40f573f7a368249accb5dc6f5
Michał Szynkiewicz <michal.l.szynkiewicz@gmail.com>
2020-11-30 22:43:37 +0100

    reactive-messaging-http quickstart

---
+++ reactive-messaging-http-quickstart/src/main/java/org/acme/reactivehttp/Cost.java
@@ -0,0 +1,22 @@
+package org.acme.reactivehttp;
+
+public class Cost {
+    private double value;
+    private String currency;
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+}
