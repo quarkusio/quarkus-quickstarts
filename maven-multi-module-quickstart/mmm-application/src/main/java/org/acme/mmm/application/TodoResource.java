45e01b19210c8e5879f900686a1004683ffc0833
Martin Maier <martin.maier@ippen-digital.de>
2020-12-14 01:30:33 +0100

    fixed star-imports

--- maven-multi-module-quickstart/mmm-application/src/main/java/org/acme/mmm/application/TodoResource.java
+++ maven-multi-module-quickstart/mmm-application/src/main/java/org/acme/mmm/application/TodoResource.java
@@ -4,7 +4,13 @@ import org.acme.mmm.domain.Todo;
 import org.acme.mmm.domain.TodoRepostory;
 
 import javax.inject.Inject;
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
