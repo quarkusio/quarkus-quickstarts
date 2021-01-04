d09321c92dfaaa5e77da12cd5c1a8fde9dab8d28
Yoann Rodière <yoann@hibernate.org>
2021-01-04 14:53:50 +0100

    hsearch-es: Use a named CDI bean for the analysis configurer
    Signed-off-by: Yoann Rodière <yoann@hibernate.org>

--- hibernate-search-orm-elasticsearch-quickstart/src/main/java/org/acme/hibernate/search/elasticsearch/config/AnalysisConfigurer.java
+++ hibernate-search-orm-elasticsearch-quickstart/src/main/java/org/acme/hibernate/search/elasticsearch/config/AnalysisConfigurer.java
@@ -3,6 +3,11 @@ package org.acme.hibernate.search.elasticsearch.config;
 import org.hibernate.search.backend.elasticsearch.analysis.ElasticsearchAnalysisConfigurationContext;
 import org.hibernate.search.backend.elasticsearch.analysis.ElasticsearchAnalysisConfigurer;
 
+import javax.enterprise.context.Dependent;
+import javax.inject.Named;
+
+@Dependent
+@Named("myAnalysisConfigurer")
 public class AnalysisConfigurer implements ElasticsearchAnalysisConfigurer {
 
     @Override
