f01964ead70364382dcce75c75eaa62bd25b4be8
Ed Mangini <me@emangini.com>
2021-02-27 15:28:49 -0500

    Removed logging. May be causing intermittent timeouts in tests

--- amazon-ses-quickstart/src/main/java/org/acme/ses/QuarkusSesAsyncResource.java
+++ amazon-ses-quickstart/src/main/java/org/acme/ses/QuarkusSesAsyncResource.java
@@ -7,17 +7,13 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
-import lombok.extern.jbosslog.JBossLog;
 import org.acme.ses.model.Email;
 import software.amazon.awssdk.services.ses.SesAsyncClient;
-import software.amazon.awssdk.services.ses.model.Message;
-import software.amazon.awssdk.services.ses.model.SendEmailRequest;
 import software.amazon.awssdk.services.ses.model.SendEmailResponse;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import static javax.ws.rs.core.MediaType.TEXT_PLAIN;
 
-@JBossLog
 @Path("/async")
 @Produces(TEXT_PLAIN)
 @Consumes(APPLICATION_JSON)
@@ -30,7 +26,6 @@ public class QuarkusSesAsyncResource {
     @Path("email")
     public Uni<String> encrypt(Email data) {
 
-        log.infof("Encrypting Email: From: %s To: %s...asynchronously", data.getFrom(), data.getTo());
         return Uni.createFrom().completionStage(
                 ses.sendEmail(EmailHelper.createRequest(data)))
                 .onItem().transform(SendEmailResponse::messageId);
--- amazon-ses-quickstart/src/main/java/org/acme/ses/QuarkusSesSyncResource.java
+++ amazon-ses-quickstart/src/main/java/org/acme/ses/QuarkusSesSyncResource.java
@@ -5,18 +5,13 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 
-import lombok.extern.jbosslog.JBossLog;
 import org.acme.ses.model.Email;
 import software.amazon.awssdk.services.ses.SesClient;
-import software.amazon.awssdk.services.ses.model.Message;
-import software.amazon.awssdk.services.ses.model.SendEmailRequest;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import static javax.ws.rs.core.MediaType.TEXT_PLAIN;
 
-@JBossLog
 @Path("/sync")
 @Produces(TEXT_PLAIN)
 @Consumes(APPLICATION_JSON)
@@ -29,7 +24,6 @@ public class QuarkusSesSyncResource {
     @Path("email")
     public String encrypt(Email data) {
 
-        log.infof("Encrypting Email: From: %s To: %s...synchronously", data.getFrom(), data.getTo());
         return ses.sendEmail(EmailHelper.createRequest(data))
                 .messageId();
     }
