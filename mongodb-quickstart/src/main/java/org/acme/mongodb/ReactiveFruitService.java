3200e9c5f229ec1b44bea89121061631277619d5
Ed Mangini <me@emangini.com>
2021-03-03 14:55:52 -0500

    mongo-quickstart: fixed module (it returns/sets ids properly now), added Tests, and added a docker-compose.yaml

--- mongodb-quickstart/src/main/java/org/acme/mongodb/ReactiveFruitService.java
+++ mongodb-quickstart/src/main/java/org/acme/mongodb/ReactiveFruitService.java
@@ -1,6 +1,7 @@
 package org.acme.mongodb;
 
 import java.util.List;
+import java.util.UUID;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
@@ -22,14 +23,22 @@ public class ReactiveFruitService {
                 .map(doc -> {
                     Fruit fruit = new Fruit();
                     fruit.setName(doc.getString("name"));
+                    fruit.setId(doc.getString("id"));
                     fruit.setDescription(doc.getString("description"));
                     return fruit;
-                }).collectItems().asList();
+                }).collect().asList();
     }
 
     public Uni<Void> add(Fruit fruit) {
+
+        /*
+            This is usually going to be done in the data layer itself (See FruitCodec for a better example)
+         */
+        fruit.setId(UUID.randomUUID().toString());
+
         Document document = new Document()
                 .append("name", fruit.getName())
+                .append("id", fruit.getId())
                 .append("description", fruit.getDescription());
         return getCollection().insertOne(document)
                 .onItem().ignore().andContinueWithNull();
