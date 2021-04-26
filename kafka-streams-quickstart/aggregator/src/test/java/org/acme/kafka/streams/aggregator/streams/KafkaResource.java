55894d69d1687f379958e4475fe5d69a37cd94ef
Clement Escoffier <clement.escoffier@gmail.com>
2021-04-26 19:15:15 +0200

    Replace Debezium with Test Containers

--- kafka-streams-quickstart/aggregator/src/test/java/org/acme/kafka/streams/aggregator/streams/KafkaResource.java
+++ kafka-streams-quickstart/aggregator/src/test/java/org/acme/kafka/streams/aggregator/streams/KafkaResource.java
@@ -1,43 +1,28 @@
 package org.acme.kafka.streams.aggregator.streams;
 
-import io.debezium.kafka.KafkaCluster;
-import io.debezium.util.Testing;
 import io.quarkus.test.common.QuarkusTestResourceLifecycleManager;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.utility.DockerImageName;
 
-import java.io.File;
-import java.lang.reflect.Field;
 import java.util.Collections;
 import java.util.Map;
-import java.util.Properties;
 
-public class KafkaResource implements QuarkusTestResourceLifecycleManager  {
+public class KafkaResource implements QuarkusTestResourceLifecycleManager {
 
-    private KafkaCluster kafka;
+    private final static KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:5.4.3"));
 
     @Override
     public Map<String, String> start() {
-        try {
-            Properties props = new Properties();
-            props.setProperty("zookeeper.connection.timeout.ms", "45000");
-            props.setProperty("group.min.session.timeout.ms", "100");
-            File directory = Testing.Files.createTestingDirectory("kafka-data", true);
-            kafka = new KafkaCluster().withPorts(2182, 9092)
-                    .addBrokers(1)
-                    .usingDirectory(directory)
-                    .deleteDataUponShutdown(true)
-                    .withKafkaConfiguration(props)
-                    .deleteDataPriorToStartup(true)
-                    .startup();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        return Collections.emptyMap();
+        kafka.start();
+        return Collections.singletonMap("quarkus.kafka-streams.bootstrap-servers", kafka.getBootstrapServers());
     }
 
     @Override
     public void stop() {
-        if (kafka != null) {
-            kafka.shutdown();
-        }
+        kafka.close();
+    }
+
+    public static String getBootstrapServers() {
+        return kafka.getBootstrapServers();
     }
 }
