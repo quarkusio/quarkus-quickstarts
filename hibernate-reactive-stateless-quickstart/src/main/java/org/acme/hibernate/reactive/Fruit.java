f8ca29b3ac8919c993d236975549415f78e746f8
Gavin King <gavin@hibernate.org>
2021-05-26 12:04:16 +0200

    create HR example using StatelessSession

---
+++ hibernate-reactive-stateless-quickstart/src/main/java/org/acme/hibernate/reactive/Fruit.java
@@ -0,0 +1,44 @@
+package org.acme.hibernate.reactive;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "known_fruits")
+public class Fruit {
+
+    @Id
+    @SequenceGenerator(name = "fruitsSequence", sequenceName = "known_fruits_id_seq", allocationSize = 10, initialValue = 10)
+    @GeneratedValue(generator = "fruitsSequence")
+    private Integer id;
+
+    @Column(length = 40, unique = true)
+    private String name;
+
+    Fruit() {}
+
+    public Fruit(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Fruit{" + id + "," + name + '}';
+    }
+}
