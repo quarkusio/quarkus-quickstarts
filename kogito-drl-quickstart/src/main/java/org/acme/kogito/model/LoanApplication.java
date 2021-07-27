6c80fcc65b6ac4aeea9706cf878ad9f51b9c0c75
Luca Molteni <volothamp@gmail.com>
2021-07-27 14:38:58 +0200

    Quickstart for KOGITO-5289 Kogito DRL guide

---
+++ kogito-drl-quickstart/src/main/java/org/acme/kogito/model/LoanApplication.java
@@ -0,0 +1,61 @@
+package org.acme.kogito.model;
+
+public class LoanApplication {
+
+    private String id;
+    private Applicant applicant;
+    private int amount;
+    private int deposit;
+    private boolean approved = false;
+
+    public LoanApplication() {
+    }
+
+    public LoanApplication(String id, Applicant applicant,
+                           int amount, int deposit) {
+        this.id = id;
+        this.applicant = applicant;
+        this.amount = amount;
+        this.deposit = deposit;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Applicant getApplicant() {
+        return applicant;
+    }
+
+    public void setApplicant(Applicant applicant) {
+        this.applicant = applicant;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public int getDeposit() {
+        return deposit;
+    }
+
+    public void setDeposit(int deposit) {
+        this.deposit = deposit;
+    }
+
+    public boolean isApproved() {
+        return approved;
+    }
+
+    public void setApproved(boolean approved) {
+        this.approved = approved;
+    }
+}
