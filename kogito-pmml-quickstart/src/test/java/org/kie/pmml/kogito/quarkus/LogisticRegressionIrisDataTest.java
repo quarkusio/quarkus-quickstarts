167379b571a5f68f059d544604a90a28fc6383d5
Guillaume Smet <guillaume.smet@gmail.com>
2021-08-02 10:53:45 +0200

    Fix a few issues in kogito-pmml-quickstart

--- kogito-pmml-quickstart/src/test/java/org/kie/pmml/kogito/quarkus/LogisticRegressionIrisDataTest.java
+++ kogito-pmml-quickstart/src/test/java/org/kie/pmml/kogito/quarkus/LogisticRegressionIrisDataTest.java
@@ -1,31 +1,19 @@
-/*
- * Copyright 2020 Red Hat, Inc. and/or its affiliates.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.kie.pmml.kogito.quarkus;
 
-import io.quarkus.test.junit.QuarkusTest;
-import io.restassured.http.ContentType;
-import org.hamcrest.core.IsNull;
-import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Collections;
 import java.util.Map;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.is;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
 
 @QuarkusTest
 class LogisticRegressionIrisDataTest {
@@ -49,12 +37,12 @@ class LogisticRegressionIrisDataTest {
                 .post(BASE_PATH)
                 .then()
                 .statusCode(200)
-                .body(TARGET, is(52.5f));
+                .body(TARGET, is("virginica"));
     }
 
     @Test
     void testEvaluateLogisticRegressionIrisDataDescriptive() {
-        final Map<String, Object> expectedResultMap = Collections.singletonMap(TARGET, 52.5f);
+        final Map<String, Object> expectedResultMap = Collections.singletonMap(TARGET, "virginica");
         Object resultVariables = given()
                 .contentType(ContentType.JSON)
                 .body(INPUT_DATA)
@@ -62,9 +50,9 @@ class LogisticRegressionIrisDataTest {
                 .post(DESCRIPTIVE_PATH)
                 .then()
                 .statusCode(200)
-                .body("correlationId", is(new IsNull()))
-                .body("segmentationId", is(new IsNull()))
-                .body("segmentId", is(new IsNull()))
+                .body("correlationId", is(nullValue()))
+                .body("segmentationId", is(nullValue()))
+                .body("segmentId", is(nullValue()))
                 .body("segmentIndex", is(0)) // as JSON is not schema aware, here we assert the RAW string
                 .body("resultCode", is("OK"))
                 .body("resultObjectName", is(TARGET))
