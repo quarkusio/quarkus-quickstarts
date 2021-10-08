5c64f1167fecc8039ac18c5a02800b4d81d2722f
Loïc Mathieu <loikeseke@gmail.com>
2021-10-08 12:39:16 +0200

    Add repository example with the MongoDB with Panache quickstart

---
+++ mongodb-panache-quickstart/src/main/java/org/acme/mongodb/panache/repository/Person.java
@@ -0,0 +1,33 @@
+package org.acme.mongodb.panache.repository;
+
+import io.quarkus.mongodb.panache.MongoEntity;
+import io.quarkus.mongodb.panache.PanacheMongoEntity;
+import org.bson.codecs.pojo.annotations.BsonProperty;
+import org.bson.types.ObjectId;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@MongoEntity(collection = "ThePerson")
+public class Person {
+    public ObjectId id;
+
+    public String name;
+
+    // will be persisted as a 'birth' field in MongoDB
+    @BsonProperty("birth")
+    public LocalDate birthDate;
+
+    public Status status;
+
+    // return name as lowercase in the model
+    public String getName() {
+        return name.toLowerCase();
+    }
+
+    // store all names in uppercase in the DB
+    public void setName(String name) {
+        this.name = name.toUpperCase();
+    }
+
+}
