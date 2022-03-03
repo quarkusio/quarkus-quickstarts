920ab3d826d863cfcba091870aef51b52bbd2f1e
Clement Escoffier <clement@apache.org>
2022-03-03 15:29:53 +0100

    Migrate the context propagation quickstart to RESTEasy Reactive

--- context-propagation-quickstart/src/main/java/org/acme/context/EmitterResource.java
+++ context-propagation-quickstart/src/main/java/org/acme/context/EmitterResource.java
@@ -9,8 +9,9 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import io.smallrye.mutiny.infrastructure.Infrastructure;
 import org.eclipse.microprofile.reactive.messaging.Channel;
-import org.jboss.resteasy.reactive.RestSseElementType;
+import org.jboss.resteasy.reactive.RestStreamElementType;
 import org.reactivestreams.Publisher;
 
 import io.smallrye.mutiny.Multi;
@@ -26,11 +27,14 @@ public class EmitterResource {
     @GET
     @Path("/prices")
     @Produces(MediaType.SERVER_SENT_EVENTS)
-    @RestSseElementType(MediaType.TEXT_PLAIN)
+    @RestStreamElementType(MediaType.TEXT_PLAIN)
     public Publisher<Double> prices() {
         // get the next three prices from the price stream
         return Multi.createFrom().publisher(prices)
                 .select().first(3)
+                // The items are received from the event loop, so cannot use Hibernate ORM (classic)
+                // Switch to a worker thread, the transaction will be propagated
+                .emitOn(Infrastructure.getDefaultExecutor())
                 .map(price -> {
                     // store each price before we send them
                     Price priceEntity = new Price();
