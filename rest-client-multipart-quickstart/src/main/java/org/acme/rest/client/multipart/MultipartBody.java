4d00734e8375461b40d3b8eb27de07eaa571f104
Guillaume Smet <guillaume.smet@gmail.com>
2022-03-17 22:07:34 +0100

    Go back to RESTEasy Classic for rest-client-multipart-quickstart
    If we want one for RESTEasy Reactive, let's do a specific one.

--- rest-client-multipart-quickstart/src/main/java/org/acme/rest/client/multipart/MultipartBody.java
+++ rest-client-multipart-quickstart/src/main/java/org/acme/rest/client/multipart/MultipartBody.java
@@ -5,7 +5,7 @@ import java.io.InputStream;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.core.MediaType;
 
-import org.jboss.resteasy.reactive.PartType;
+import org.jboss.resteasy.annotations.providers.multipart.PartType;
 
 public class MultipartBody {
 
--- tests-with-coverage-quickstart/src/main/java/org/acme/testcoverage/GreetingResource.java
+++ tests-with-coverage-quickstart/src/main/java/org/acme/testcoverage/GreetingResource.java
@@ -3,6 +3,7 @@ package org.acme.testcoverage;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -19,7 +20,7 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     @Path("/greeting/{name}")
-    public String greeting(String name) {
+    public String greeting(@PathParam("name") String name) {
         return service.greeting(name);
     }
 
