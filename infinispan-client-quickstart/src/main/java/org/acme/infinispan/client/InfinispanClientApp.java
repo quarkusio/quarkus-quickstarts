5903511962dcb41513b3d11e7e2d383fe346f44a
Katia Aresti <karesti@redhat.com>
2022-09-09 16:06:22 +0200

    Infinispan create cache with minimal defaults

--- infinispan-client-quickstart/src/main/java/org/acme/infinispan/client/InfinispanClientApp.java
+++ infinispan-client-quickstart/src/main/java/org/acme/infinispan/client/InfinispanClientApp.java
@@ -4,24 +4,21 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 
+import io.quarkus.infinispan.client.Remote;
 import io.quarkus.logging.Log;
 import org.infinispan.client.hotrod.RemoteCache;
-import org.infinispan.client.hotrod.RemoteCacheManager;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import io.quarkus.runtime.StartupEvent;
 
 @ApplicationScoped
 public class InfinispanClientApp {
 
-
     @Inject
-    RemoteCacheManager cacheManager;
+    @Remote("mycache")
+    RemoteCache<String, String> cache;
 
     void onStart(@Observes StartupEvent ev) {
         Log.info("Get cache named mycache and add put a key/value");
-        RemoteCache<Object, Object> cache = cacheManager.getCache("mycache");
         cache.put("hello", "Hello World, Infinispan is up!");
     }
 }
