e2cab58973dfcd3c8a65c93acfdc82a77ad4150c
Guillaume Smet <guillaume.smet@gmail.com>
2023-02-04 10:19:15 +0100

    Delete kafka-avro-schema-quickstart
    It is not supported anymore.

--- kafka-avro-schema-quickstart/README.md
+++
@@ -1,1 +0,0 @@
-The corresponding guide: https://quarkus.io/guides/quarkus-schema-registry-avro
--- kafka-avro-schema-quickstart/src/test/java/org/acme/kafka/MovieResourceIT.java
+++
@@ -1,9 +0,0 @@
-package org.acme.kafka;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class MovieResourceIT extends MovieResourceTest {
-
-    // Execute the same tests but in native mode.
-}
