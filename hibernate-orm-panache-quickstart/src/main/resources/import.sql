e681d80c3b9d9be3a2a7648ea217f287d0cad6df
Sanne Grinovero <sanne@hibernate.org>
2023-03-01 18:54:10 +0000

    Adapt hibernate-orm-panache-quickstart to Hibernate ORM 6.2

--- hibernate-orm-panache-quickstart/src/main/resources/import.sql
+++ hibernate-orm-panache-quickstart/src/main/resources/import.sql
@@ -1,8 +1,11 @@
-INSERT INTO fruitEntity(id, name) VALUES (nextval('hibernate_sequence'), 'Cherry');
-INSERT INTO fruitEntity(id, name) VALUES (nextval('hibernate_sequence'), 'Apple');
-INSERT INTO fruitEntity(id, name) VALUES (nextval('hibernate_sequence'), 'Banana');
+INSERT INTO fruitEntity(id, name) VALUES (1, 'Cherry');
+INSERT INTO fruitEntity(id, name) VALUES (2, 'Apple');
+INSERT INTO fruitEntity(id, name) VALUES (3, 'Banana');
+alter sequence Fruit_SEQ restart with 4;
 
 -- force using the same if for entity and repository to facilitate testing
 INSERT INTO fruit(id, name) VALUES (1, 'Cherry');
 INSERT INTO fruit(id, name) VALUES (2, 'Apple');
 INSERT INTO fruit(id, name) VALUES (3, 'Banana');
+alter sequence FruitEntity_SEQ restart with 4;
+
