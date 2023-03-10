9d04c760641d308be1ed2a6fc158c89baacd92f8
Guillaume Smet <guillaume.smet@gmail.com>
2023-03-10 11:26:20 +0100

    Remove the Kogito quickstarts

--- kogito-dmn-quickstart/src/test/java/testscenario/KogitoScenarioJunitActivatorTest.java
+++
@@ -1,5 +0,0 @@
-package testscenario;
-
-@org.junit.runner.RunWith(org.kogito.scenariosimulation.runner.KogitoJunitActivator.class)
-public class KogitoScenarioJunitActivatorTest {
-}
--- kogito-drl-quickstart/src/test/java/org/acme/kogito/LoanApplicationInGraalIT.java
+++
@@ -1,8 +0,0 @@
-package org.acme.kogito;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class LoanApplicationInGraalIT extends LoanApplicationTest {
-
-}
