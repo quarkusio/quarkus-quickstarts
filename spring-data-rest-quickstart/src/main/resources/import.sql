1cce4732bad55733c9208296d2a3e18ae0abf906
Yoann Rodière <yoann@hibernate.org>
2023-03-27 17:57:18 +0200

    Rework sequence handling in import scripts
    * to be more consistent
    * to adapt to Hibernate ORM 6 changes

--- spring-data-rest-quickstart/src/main/resources/import.sql
+++ spring-data-rest-quickstart/src/main/resources/import.sql
@@ -3,3 +3,4 @@ INSERT INTO fruit(id, name, color) VALUES (2, 'Apple', 'Red');
 INSERT INTO fruit(id, name, color) VALUES (3, 'Banana', 'Yellow');
 INSERT INTO fruit(id, name, color) VALUES (4, 'Avocado', 'Green');
 INSERT INTO fruit(id, name, color) VALUES (5, 'Strawberry', 'Red');
+ALTER SEQUENCE fruit_seq RESTART WITH 6;
