9ac73f7834a0d2cb5323d70590d724c05c2deb51
Jakub Jedlicka <jjedlick@redhat.com>
2023-07-12 17:17:59 +0200

    Add missing documentation and enhance test coverage for getting-started-reactive-crud.

--- getting-started-reactive-crud/src/test/java/org/acme/reactive/crud/FruitResourceTest.java
+++ getting-started-reactive-crud/src/test/java/org/acme/reactive/crud/FruitResourceTest.java
@@ -4,15 +4,22 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.core.IsNot.not;
 
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.TestMethodOrder;
 
 @QuarkusTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class FruitResourceTest {
+    private static String newFruit = "Lemon";
+    private static String updatedFruit = "Pitaya";
 
     @Test
-    public void testListAllFruits() {
+    @Order(1)
+    public void testAddFruit() {
         //List all, should have all 4 fruits the database has initially:
         given()
                 .when().get("/fruits")
@@ -24,22 +31,113 @@ class FruitResourceTest {
                         containsString("Pomelo"),
                         containsString("Lychee"));
 
-        //Delete the Kiwi:
+        // Add new fruit
         given()
-                .when().delete("/fruits/1")
+                .when().contentType("application/json")
+                .body("{\"name\":\"" + newFruit + "\"}")
+                .post("/fruits")
                 .then()
-                .statusCode(204);
+                .statusCode(201);
 
-        //List all, Kiwi should be missing now:
+        //List all, should have 4 fruits the database has initially and 1 added fruit:
         given()
                 .when().get("/fruits")
                 .then()
                 .statusCode(200)
                 .body(
-                        not(containsString("Kiwi")),
+                        containsString("Kiwi"),
                         containsString("Durian"),
                         containsString("Pomelo"),
-                        containsString("Lychee"));
+                        containsString("Lychee"),
+                        containsString(newFruit));
+    }
+
+    @Test
+    @Order(2)
+    public void testUpdateFruit() {
+        //List all, should have 4 fruits the database has initially and 1 added fruit:
+        given()
+                .when().get("/fruits")
+                .then()
+                .statusCode(200)
+                .body(
+                        containsString("Kiwi"),
+                        containsString("Durian"),
+                        containsString("Pomelo"),
+                        containsString("Lychee"),
+                        containsString(newFruit));
+
+        // Update name of added fruit
+        given()
+                .when().contentType("application/json")
+                .body("{\"id\":5, \"name\":\"" + updatedFruit + "\"}")
+                .put("/fruits/5")
+                .then()
+                .statusCode(200);
+
+        //List all, should have 4 fruits the database has initially and 1 updated fruit:
+        given()
+                .when().get("/fruits")
+                .then()
+                .statusCode(200)
+                .body(
+                        containsString("Kiwi"),
+                        containsString("Durian"),
+                        containsString("Pomelo"),
+                        containsString("Lychee"),
+                        containsString(updatedFruit));
+    }
+
+    @Test
+    @Order(3)
+    public void testListAllFruits() {
+        //List all, should have 5 fruits the database has initially:
+        given()
+                .when().get("/fruits")
+                .then()
+                .statusCode(200)
+                .body(
+                        containsString("Kiwi"),
+                        containsString("Durian"),
+                        containsString("Pomelo"),
+                        containsString("Lychee"),
+                        containsString(updatedFruit));
+
+        //Delete the Pitaya:
+        given()
+                .when().delete("/fruits/5")
+                .then()
+                .statusCode(204);
+
+        //List all, Pitaya should be missing now:
+        given()
+                .when().get("/fruits")
+                .then()
+                .statusCode(200)
+                .body(
+                        containsString("Kiwi"),
+                        containsString("Durian"),
+                        containsString("Pomelo"),
+                        containsString("Lychee"),
+                        not(containsString(updatedFruit)));
+    }
+
+    @Test
+    public void testNotFoundForDelete() {
+        given()
+                .when().delete("/fruits/3939")
+                .then()
+                .statusCode(404);
+    }
+
+    @Test
+    public void testNotFoundForUpdate() {
+        given()
+                .when().contentType("application/json")
+                .body("{\"id\":3939, \"name\":\"" + updatedFruit + "\"}")
+                .put("/fruits/3939")
+                .then()
+                .statusCode(404);
     }
 
 }
