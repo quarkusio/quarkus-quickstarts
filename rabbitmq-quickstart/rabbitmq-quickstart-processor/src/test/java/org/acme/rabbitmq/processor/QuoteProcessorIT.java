7d9eae040361abfcfbd482d211a10371720fc185
Georgios Andrianakis <geoand@gmail.com>
2023-09-13 14:58:33 +0300

    Remove invalid QuoteProcessorIT
    This test does not work because
    injection into ITs is not supported

--- rabbitmq-quickstart/rabbitmq-quickstart-processor/src/test/java/org/acme/rabbitmq/processor/QuoteProcessorIT.java
+++
@@ -1,8 +0,0 @@
-package org.acme.rabbitmq.processor;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class QuoteProcessorIT extends QuoteProcessorTest {
-
-}
