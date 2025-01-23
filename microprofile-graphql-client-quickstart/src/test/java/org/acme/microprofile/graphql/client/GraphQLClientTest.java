496bf3887a119eb9041354cd348b9be59fa29c63
Rostislav Svoboda <rsvoboda@redhat.com>
2025-01-23 09:48:13 +0100

    Enable microprofile-graphql-client-quickstart, update netlify URL
    Server swapi.dev is up again so the netlify application works well again
    Update of GrapQL endpoint to https://swapi-graphql.netlify.app/graphql

--- microprofile-graphql-client-quickstart/src/test/java/org/acme/microprofile/graphql/client/GraphQLClientTest.java
+++ microprofile-graphql-client-quickstart/src/test/java/org/acme/microprofile/graphql/client/GraphQLClientTest.java
@@ -1,7 +1,6 @@
 package org.acme.microprofile.graphql.client;
 
 import io.quarkus.test.junit.QuarkusTest;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -10,7 +9,6 @@ import static io.restassured.RestAssured.get;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @QuarkusTest
-@Disabled("Blocked by https://github.com/quarkusio/quarkus/issues/45334")
 public class GraphQLClientTest {
 
     @Test
