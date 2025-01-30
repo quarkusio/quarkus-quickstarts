e435a3f1b36e4bc58fef0abe64f51f2037bbf736
Tobias Haindl <t.haindl@gmx.net>
2025-01-30 09:55:25 +0100

    Fix warning by adding PersistenceUnitExtension

--- hibernate-orm-multi-tenancy-database-quickstart/src/main/java/org/acme/hibernate/orm/CustomTenantResolver.java
+++ hibernate-orm-multi-tenancy-database-quickstart/src/main/java/org/acme/hibernate/orm/CustomTenantResolver.java
@@ -3,6 +3,7 @@ package org.acme.hibernate.orm;
 import org.jboss.logging.Logger;
 
 import io.quarkus.arc.Unremovable;
+import io.quarkus.hibernate.orm.PersistenceUnitExtension;
 import io.quarkus.hibernate.orm.runtime.tenant.TenantResolver;
 import io.vertx.ext.web.RoutingContext;
 import jakarta.enterprise.context.RequestScoped;
@@ -10,6 +11,7 @@ import jakarta.inject.Inject;
 
 @RequestScoped
 @Unremovable
+@PersistenceUnitExtension
 public class CustomTenantResolver implements TenantResolver {
 
     private static final Logger LOG = Logger.getLogger(CustomTenantResolver.class);
