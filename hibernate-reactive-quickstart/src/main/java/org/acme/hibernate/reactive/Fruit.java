dc3f090c5fa6745dd426ee5e14e7d3321111af3d
Luca Molteni <volothamp@gmail.com>
2025-04-08 16:17:12 +0200

    Reviewd reactive

--- hibernate-reactive-quickstart/src/main/java/org/acme/hibernate/reactive/Fruit.java
+++ hibernate-reactive-quickstart/src/main/java/org/acme/hibernate/reactive/Fruit.java
@@ -10,13 +10,11 @@ import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "known_fruits")
 @NamedQuery(name = "Fruits.findAll", query = "SELECT f FROM Fruit f ORDER BY f.name")
 public class Fruit {
 
     @Id
-    @SequenceGenerator(name = "fruitsSequence", sequenceName = "known_fruits_id_seq", allocationSize = 1, initialValue = 10)
-    @GeneratedValue(generator = "fruitsSequence")
+    @GeneratedValue
     private Integer id;
 
     @Column(length = 40, unique = true)
