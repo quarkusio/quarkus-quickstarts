d8cf4cbb089417ece41eb0e22394712f46590c53
Auri Munoz <amunozhe@redhat.com>
2025-05-09 17:33:02 +0200

    Add a couple of Stork registration quickstarts
    use parent version

---
+++ stork-programmatic-custom-registration-quickstart/src/main/java/org/acme/services/Registration.java
@@ -0,0 +1,24 @@
+package org.acme.services;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.event.Observes;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import io.quarkus.runtime.StartupEvent;
+import io.smallrye.stork.Stork;
+import io.vertx.mutiny.core.Vertx;
+
+@ApplicationScoped
+public class Registration {
+
+    /**
+     * Register our two services using custom registrar.
+     *
+     * Note: this method is called on a worker thread, and so it is allowed to block.
+     */
+    public void init(@Observes StartupEvent ev, Vertx vertx) {
+        Stork.getInstance().getService("my-service").registerInstance("my-service", "localhost",
+                9000);
+    }
+}
