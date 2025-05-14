fde28915ab2c9137d0f327a1b78a5382e607f22d
Auri Munoz <amunozhe@redhat.com>
2025-05-14 14:59:00 +0200

    refactor

--- stork-registration-quickstart/src/main/java/org/acme/services/Registration.java
+++ stork-registration-quickstart/src/main/java/org/acme/services/Registration.java
@@ -7,6 +7,10 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.event.Observes;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
+import java.util.concurrent.CountDownLatch;
+
+import static io.smallrye.mutiny.helpers.Subscriptions.fail;
+
 @ApplicationScoped
 public class Registration {
 
@@ -19,7 +23,9 @@ public class Registration {
      * Note: this method is called on a worker thread, and so it is allowed to block.
      */
     public void init(@Observes StartupEvent ev, Vertx vertx) {
-        Stork.getInstance().getService("my-service").getServiceRegistrar().registerServiceInstance("my-service", "localhost",
-                red);
+        CountDownLatch registrationLatch = new CountDownLatch(1);
+        Stork.getInstance().getService("my-service").getServiceRegistrar().registerServiceInstance("red", "localhost",
+                red).subscribe()
+                .with(success -> registrationLatch.countDown(), failure -> System.out.println("Registration failed"));;
     }
 }
