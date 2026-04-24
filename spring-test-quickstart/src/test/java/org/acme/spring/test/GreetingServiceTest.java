e2068e16f261831b000739523d15a813364d9f54
Aurea Munoz <devauriverse@gmail.com>
2026-04-24 16:25:16 +0200

    code review

--- spring-test-quickstart/src/test/java/org/acme/spring/test/GreetingServiceTest.java
+++ spring-test-quickstart/src/test/java/org/acme/spring/test/GreetingServiceTest.java
@@ -5,12 +5,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import jakarta.inject.Inject;
 import org.junit.jupiter.api.Test;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class GreetingServiceTest {
 
-    @Inject
+    @Autowired
     GreetingService greetingService;
 
     @Test
