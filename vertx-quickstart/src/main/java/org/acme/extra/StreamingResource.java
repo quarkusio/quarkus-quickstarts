8370ba74a5a4477311febb5ce0cf7bf8eab1054c
Guillaume Smet <guillaume.smet@gmail.com>
2026-07-22 17:07:57 +0200

    Adjust the Vert.x quickstart for Quarkus 4

--- vertx-quickstart/src/main/java/org/acme/extra/StreamingResource.java
+++ vertx-quickstart/src/main/java/org/acme/extra/StreamingResource.java
@@ -1,26 +1,23 @@
 package org.acme.extra;
 
+import java.time.Duration;
 import java.util.Date;
 
-import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
 import io.smallrye.mutiny.Multi;
-import io.vertx.mutiny.core.Vertx;
 
 @Path("/hello")
 public class StreamingResource {
-
-    @Inject
-    Vertx vertx;
 
     @GET
     @Produces(MediaType.SERVER_SENT_EVENTS)
     @Path("{name}/streaming")
     public Multi<String> greeting(String name) {
-        return vertx.periodicStream(2000).toMulti().map(l -> String.format("Hello %s! (%s)%n", name, new Date()));
+        return Multi.createFrom().ticks().every(Duration.ofMillis(2000))
+                .map(l -> String.format("Hello %s! (%s)%n", name, new Date()));
     }
 }
